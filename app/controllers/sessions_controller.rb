35b19dea24d9718c3a8ec6e2b3096accc8f7140f
Adam Coffman <acoffman@genome.wustl.edu>
2014-08-20 13:04:07 -0500

    add redirection to origin url on authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,14 +11,15 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
     authorization = Authorization.find_or_create_from_omniauth(auth_hash)
+    redirect_path = get_redirect_path
 
     if signed_in?
       if authorization.user == current_user
-        redirect_to root_url, notice: 'Already linked that account!'
+        redirect_to redirect_path, notice: 'Already linked that account!'
       else
         authorization.user = current_user
         authorization.save
-        redirect_to root_url, notice: 'Successfully linked new account'
+        redirect_to redirect_path, notice: 'Successfully linked new account'
       end
     else
       if authorization.user.present?
@@ -29,12 +30,21 @@ class SessionsController < ApplicationController
         authorization.save
         sign_in(user)
       end
-      redirect_to root_url, notice: 'Signed in!'
+      redirect_to redirect_path, notice: 'Signed in!'
     end
   end
 
   def destroy
     sign_out
     redirect_to root_url
+  end
+
+  private
+  def get_redirect_path
+    if origin = request.env['omniauth.origin']
+      origin
+    else
+      root_url
+    end
   end
 end
