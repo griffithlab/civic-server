5b39179d02e043f8fc0e3aa23a3a9aa31472c776
Adam Coffman <acoffman@genome.wustl.edu>
2014-09-02 17:27:37 -0500

    add pathway relationships

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -1,10 +1,12 @@
 class Gene < ActiveRecord::Base
   has_many :events
   has_many :category_genes
+  has_many :gene_pathways
   has_many :gene_protein_motifs
   has_many :gene_protein_functions
 
   has_many :categories, through: :category_genes
+  has_many :pathways, through: :gene_pathways
   has_many :protein_motifs, through: :gene_protein_motifs
   has_many :protein_functions, through: :gene_protein_functions
 end
