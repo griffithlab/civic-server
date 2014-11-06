780f0ed459c24460016c8efd3db075f5c95c9acc
Adam Coffman <acoffman@genome.wustl.edu>
2014-11-06 16:05:34 -0600

    pluralize all the detail lists

--- app/presenters/gene_presenter.rb
+++ app/presenters/gene_presenter.rb
@@ -13,9 +13,9 @@ class GenePresenter
       variants: @gene.variants.map(&:name),
       variant_groups: @gene.variant_groups.map(&:name),
       details: {
-          gene_category: @gene.categories.map(&:name),
-          gene_pathway: @gene.pathways.map(&:name),
-          protein_motif: @gene.protein_motifs.map(&:name),
+          gene_categories: @gene.categories.map(&:name),
+          gene_pathways: @gene.pathways.map(&:name),
+          protein_motifs: @gene.protein_motifs.map(&:name),
           protein_functions: @gene.protein_functions.map(&:name)
       }
     }.merge(errors)
