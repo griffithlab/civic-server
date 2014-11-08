1e723130c5945d87cf5e4b364326c5349ab683e2
Adam Coffman <acoffman@genome.wustl.edu>
2014-11-08 09:47:21 -0800

    disable gene validation for now

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -13,11 +13,11 @@ class Gene < ActiveRecord::Base
   has_many :protein_motifs, through: :gene_protein_motifs
   has_many :protein_functions, through: :gene_protein_functions
 
-  validates :name, presence: true, uniqueness: true
-  validates :entrez_id, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :official_name, presence: true
-  validates :clinical_description, presence: true
+  #validates :name, presence: true, uniqueness: true
+  #validates :entrez_id, presence: true, uniqueness: true
+  #validates :description, presence: true
+  #validates :official_name, presence: true
+  #validates :clinical_description, presence: true
 
   audited except: [:created_at, :updated_at], allow_mass_assignment: true
 
