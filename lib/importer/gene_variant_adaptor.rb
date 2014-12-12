01d1a797ff23565e61c2800445b1b6d3d4dd66d9
Adam Coffman <acoffman@genome.wustl.edu>
2014-12-12 12:32:16 -0600

    remove uncalled methods

--- lib/importer/gene_variant_adaptor.rb
+++ lib/importer/gene_variant_adaptor.rb
@@ -7,49 +7,14 @@ module Importer
     def create_entities_for_row(row)
       gene = EntityMaps::Gene.get_entity_from_row(row)
       gene.save
-      #make_category(gene, row)
       make_variant(gene, row)
-      #make_pathway(gene, row)
-      #make_protein_motifs(gene, row)
-      #make_protein_function(gene, row)
     end
 
     private
-    def make_category(gene, row)
-      category = EntityMaps::Category.get_entity_from_row(row)
-      category.save
-      category_gene = EntityMaps::CategoryGene.get_entity_from_row(row, gene: gene, category: category)
-      category_gene.save
-    end
-
     def make_variant(gene, row)
       variant = EntityMaps::Variant.get_entity_from_row(row)
       variant.gene = gene
       variant.save
-    end
-
-    def make_pathway(gene, row)
-      pathway = EntityMaps::Pathway.get_entity_from_row(row)
-      pathway.save
-      gene_pathway = EntityMaps::GenePathway.get_entity_from_row(row, gene: gene, pathway: pathway)
-      gene_pathway.save
-    end
-
-    def make_protein_motifs(gene, row)
-      protein_motifs = EntityMaps::ProteinMotif.get_entity_from_row(row)
-      protein_motifs.each do |protein_motif|
-        protein_motif.save
-        gene_protein_motif = EntityMaps::GeneProteinMotif.get_entity_from_row(row, gene: gene, protein_motif: protein_motif)
-        gene_protein_motif.save
-      end
-    end
-
-    def make_protein_function(gene, row)
-      protein_function = EntityMaps::ProteinFunction.get_entity_from_row(row)
-      protein_function.save
-
-      gene_protein_function = EntityMaps::GeneProteinFunction.get_entity_from_row(row, gene: gene, protein_function: protein_function)
-      gene_protein_function.save
     end
   end
 end
