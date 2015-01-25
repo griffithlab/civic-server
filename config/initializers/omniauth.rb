d5c53996ad4bf42247c0cfcecfd1b172000c1116
Adam Coffman <acoffman@genome.wustl.edu>
2015-01-25 11:37:45 -0600

    optionally load oauth creds from secrets.yml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+civic_github_key = ENV['CIVIC_GITHUB_KEY'] || Rails.application.secrets.civic_github_key
+civic_github_secret = ENV['CIVIC_GITHUB_SECRET'] || Rails.application.secrets.civic_github_secret
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['CIVIC_GITHUB_KEY'], ENV['CIVIC_GITHUB_SECRET']
+  provider :github, civic_github_key, civic_github_secret
 end
