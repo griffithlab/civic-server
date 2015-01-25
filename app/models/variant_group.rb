4631dc580d01ab00ead41aebd585e42c709d0b5c
Adam Coffman <acoffman@genome.wustl.edu>
2015-01-25 11:38:55 -0600

    add view scope for variant groups

--- app/models/variant_group.rb
+++ app/models/variant_group.rb
@@ -1,4 +1,8 @@
 class VariantGroup < ActiveRecord::Base
   has_many :variant_group_variants
   has_many :variants, through: :variant_group_variants
+
+  def self.view_scope
+    includes(:variants)
+  end
 end
