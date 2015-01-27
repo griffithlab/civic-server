1ae7a2fbe136083a5f105db9c25f11ad13ab47f5
Adam Coffman <acoffman@genome.wustl.edu>
2015-01-27 15:38:29 -0600

    prefix auth path

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,8 @@ civic_github_key = ENV['CIVIC_GITHUB_KEY'] || Rails.application.secrets.civic_gi
 civic_github_secret = ENV['CIVIC_GITHUB_SECRET'] || Rails.application.secrets.civic_github_secret
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  configure do |config|
+    config.path_prefix = '/api/auth'
+  end
   provider :github, civic_github_key, civic_github_secret
 end
