37fa44346bce64b97b93b4693256c57589242ba2
Adam Coffman <acoffman@genome.wustl.edu>
2015-02-18 14:11:25 -0600

    search drug and disease names in typeahead search

--- app/presenters/variant_typeahead_results_presenter.rb
+++ app/presenters/variant_typeahead_results_presenter.rb
@@ -5,7 +5,7 @@ class VariantTypeaheadResultsPresenter
 
   def initialize(view_context)
     @view_context = view_context
-    @search_val = "%#{params[:query]}%"
+    @search_val = "#{params[:query]}%"
     @search_char_set = Set.new(params[:query].downcase.chars)
   end
 
@@ -20,6 +20,7 @@ class VariantTypeaheadResultsPresenter
   def results
     @results ||= Variant.typeahead_scope
                     .where('genes.name ILIKE :search OR variants.name ILIKE :search', search: @search_val)
+                    .where('diseases.name ILIKE :search OR drugs.name ILIKE :search', search: @search_val)
                     .limit(params[:limit])
                     .sort_by(&method(:match_val))
                     .reverse
