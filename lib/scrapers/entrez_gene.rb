036d96e4aa754f9df9db86e8a8b0847943173d43
Adam Coffman <acoffman@genome.wustl.edu>
2015-02-20 12:09:18 -0600

    entrez and pubchem scrapers

---
+++ lib/scrapers/entrez_gene.rb
@@ -0,0 +1,21 @@
+module Scrapers
+  class EntrezGene
+    def self.get_gene_symbol_from_entrez_id(entrez_id)
+      resp = Util.make_get_request(url_from_entrez_id(entrez_id))
+      extract_gene_symbol_from_response(resp)
+    rescue
+      ''
+    end
+
+    private
+    def self.url_from_entrez_id(entrez_id)
+      "http://www.ncbi.nlm.nih.gov/gene/?term=#{entrez_id}[id]&report=xml&format=text"
+    end
+
+    def self.extract_gene_symbol_from_response(text)
+      xml = Nokogiri::XML(Nokogiri::XML(text).text)
+      node = xml.at_xpath('//Gene-commentary_properties/Gene-commentary/Gene-commentary_label[text()="Official Symbol"]').parent
+      node.at_xpath('Gene-commentary_text').text
+    end
+  end
+end
