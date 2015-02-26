80a2dd029f0d74151a6af41c1303b3996e112e03
Adam Coffman <acoffman@genome.wustl.edu>
2015-02-25 20:15:25 -0600

    new data dump pre-agbt

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module CivicServer
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    config.data_dump_version = 4
+    config.data_dump_version = 5
     config.data_dump_path = File.join(Rails.root, 'db', 'data.sql')
 
     config.active_job.queue_adapter = :delayed_job
