0644bf2feb384728338237fa5ee8016fff8aedbf
Adam Coffman <acoffman@genome.wustl.edu>
2015-03-03 17:47:57 -0600

    fix test by adding gene alias fabricator

--- spec/fabricators/gene_fabricator.rb
+++ spec/fabricators/gene_fabricator.rb
@@ -4,4 +4,9 @@ Fabricator(:gene) do
   description { sequence(:gene_description) { |i| "this is a gene description #{i}" } }
   official_name { sequence(:gene_official_name) { |i| "this is a gene official name #{i}" } }
   clinical_description { sequence(:gene_clinical_description) { |i| "this is a gene clinicaldescription #{i}" } }
+  gene_aliases(count: 1) { |attrs, i| Fabricate(:gene_alias) }
+end
+
+Fabricator(:gene_alias) do
+  name { sequence(:gene_alias_name) { |i| "Gene Alias ##{i}" } }
 end
