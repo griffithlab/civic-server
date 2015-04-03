658ff4b5295b9e65623d3792c0fb66a4d1d49684
Adam Coffman <acoffman@genome.wustl.edu>
2015-04-03 13:58:26 -0500

    add mixins to variant group

--- app/models/variant_group.rb
+++ app/models/variant_group.rb
@@ -1,4 +1,9 @@
 class VariantGroup < ActiveRecord::Base
+  include Moderated
+  include Subscribable
+  acts_as_commentable
+  audited except: [:created_at, :updated_at], allow_mass_assignment: true
+
   has_many :variant_group_variants
   has_many :variants, through: :variant_group_variants
 
