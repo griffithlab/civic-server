59382acb6a72f2eed1ebbde2ba645b8cd6c7102b
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-01 14:03:00 -0500

    stub in new classes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,5 @@
 class Subscription < ActiveRecord::Base
-  validates_presence_of :user, :subscribable
+  validates_presence_of :user
 
   belongs_to :user
   belongs_to :subscribable, polymorphic: true
