3f895839663e86e9c14fe5ec8c4e340babeddd10
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-02 13:11:35 -0500

    add functionality to merge accounts

---
+++ app/jobs/merge_accounts.rb
@@ -0,0 +1,82 @@
+class MergeAccounts < ActiveJob::Base
+  attr_reader :remaining_user, :subsumed_user
+
+  def perform(remaining_user, subsumed_user)
+    @remaining_user = remaining_user
+    @subsumed_user = subsumed_user
+    ActiveRecord::Base.transaction do
+      transfer_comments
+      transfer_subscriptions
+      transfer_feed
+      transfer_roles
+      transfer_authorizations
+      transfer_revisions
+      remove_old_user
+    end
+  end
+
+  private
+  def transfer_comments
+    Comment.where(user: subsumed_user).find_each do |c|
+      c.user = remaining_user
+      c.save
+    end
+  end
+
+  def transfer_subscriptions
+    Subscription.where(user: subsumed_user).find_each do |s|
+      new_values =  {
+        user: remaining_user,
+        subscribable: s.subscribable,
+        action_type: s.action_type,
+        action_class: s.action_class
+      }
+      transfer_ownership_or_delete(s, new_values, Subscription)
+    end
+  end
+
+  def transfer_feed
+    Feed.for_user(subsumed_user).find_each do |feed_item|
+      new_values = {
+        user: remaining_user,
+        event_id: feed_item.event_id
+      }
+      transfer_ownership_or_delete(feed_item, new_values, Feed)
+    end
+  end
+
+  def transfer_roles
+    remaining_user.role_ids = (subsumed_user.role_ids + remaining_user.role_ids).uniq
+  end
+
+  def transfer_authorizations
+    Authorization.where(user: subsumed_user).find_each do |a|
+      new_values = {
+        user: remaining_user,
+        provider: a.provider,
+        uid: a.uid
+      }
+      transfer_ownership_or_delete(a, new_values, Authorization)
+    end
+  end
+
+  def transfer_revisions
+    Audited.audit_class.where(user: subsumed_user).find_each do |a|
+      a.user = remaining_user
+      a.save
+    end
+  end
+
+  def remove_old_user
+    subsumed_user.destroy
+  end
+
+  def transfer_ownership_or_delete(object, new_values, klass)
+    if klass.where(new_values).exists?
+      object.destroy
+    else
+      object.user = remaining_user
+      object.save
+    end
+  end
+end
---
+++ spec/jobs/merge_accounts_spec.rb
@@ -0,0 +1,117 @@
+require 'spec_helper'
+
+describe 'merging accounts' do
+  it 'should merge subscriptions' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+    gene = Fabricate(:gene)
+    OnSiteSubscription.create(user: remaining_user, subscribable: gene)
+    OnSiteSubscription.create(user: remaining_user, action_type: 'commented', action_class: 'Gene')
+    OnSiteSubscription.create(user: subsumed_user, subscribable: gene)
+    OnSiteSubscription.create(user: subsumed_user, action_type: 'commented', action_class: 'Gene')
+    OnSiteSubscription.create(user: subsumed_user, action_type: 'commented', action_class: 'Variant')
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(Subscription.count).to eq 3
+    expect(remaining_user.subscriptions.count).to eq 3
+  end
+
+  it 'should merge feed items' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+    gene = Fabricate(:gene)
+    event1 = Event.create(
+      action: '1',
+      originating_user: Fabricate(:user),
+      subject: gene
+    )
+    event2 = Event.create(
+      action: '2',
+      originating_user: Fabricate(:user),
+      subject: gene
+    )
+    Feed.create(user: remaining_user, acknowledged: false, event: event1)
+    Feed.create(user: subsumed_user, acknowledged: false, event: event1)
+    Feed.create(user: subsumed_user, acknowledged: false, event: event2)
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(Feed.for_user(remaining_user).count).to eq 2
+    expect(Feed.count).to eq 2
+  end
+
+  it 'it should merge roles' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+
+    remaining_user.make_admin!
+    subsumed_user.make_admin!
+    subsumed_user.make_moderator!
+
+    expect(remaining_user.roles.count).to eq 2
+    expect(subsumed_user.roles.count).to eq 3
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(remaining_user.roles.count).to eq 3
+    expect(Role.count).to eq 3
+  end
+
+  it 'should merge authorizations' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+
+    Authorization.create(user: remaining_user, provider: 'provider1', uid: '1')
+    Authorization.create(user: subsumed_user, provider: 'provider1', uid: '1')
+    Authorization.create(user: subsumed_user, provider: 'provider1', uid: '2')
+
+    expect(remaining_user.authorizations.count).to be 1
+    expect(subsumed_user.authorizations.count).to be 2
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(remaining_user.authorizations.count).to be 2
+  end
+
+  it 'should transfer comments' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+    gene = Fabricate(:gene)
+    Comment.create(commentable: gene, user: remaining_user, title: 'blah', text: 'blah')
+    Comment.create(commentable: gene, user: subsumed_user, title: 'blah', text: 'blah')
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(Comment.count).to eq 2
+    expect(Comment.where(user: remaining_user).count).to eq 2
+    expect(Comment.where(user_id: subsumed_user.id).count).to eq 0
+  end
+
+  it 'should transfer revisions' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+    gene = Fabricate(:gene)
+    Audited.audit_class.as_user(subsumed_user) do
+      gene.name = 'new name'
+      gene.save
+    end
+
+    expect(Audited.audit_class.where(user: subsumed_user).count).to eq 1
+    expect(Audited.audit_class.where(user: remaining_user).count).to eq 0
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(Audited.audit_class.where(user: subsumed_user).count).to eq 0
+    expect(Audited.audit_class.where(user: remaining_user).count).to eq 1
+  end
+
+  it 'should remove the old user' do
+    remaining_user = Fabricate(:user)
+    subsumed_user = Fabricate(:user)
+
+    MergeAccounts.new.perform(remaining_user, subsumed_user)
+
+    expect(User.where(id: subsumed_user.id).exists?).to be false
+  end
+end
