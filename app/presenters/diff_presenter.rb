ef850faa93cb9647caeecb8ef30ce2a964802e9d
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-05 13:49:35 -0500

    escape markup in diffs

--- app/presenters/diff_presenter.rb
+++ app/presenters/diff_presenter.rb
@@ -1,11 +1,13 @@
 class DiffPresenter
+  include ERB::Util
+
   def initialize(changes_hash)
     @changes_hash = changes_hash
   end
 
   def as_json(options = {})
     @changes_hash.each_with_object({}) do |(property_name, (old_value, new_value)), changes|
-      changes[property_name] = Diffy::Diff.new(old_value, new_value).to_s(:html)
+      changes[property_name] = Diffy::Diff.new(html_escape(old_value), html_escape(new_value)).to_s(:html)
     end
   end
 end
