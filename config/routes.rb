2073de26688bc719901e51f5b90c209933232732
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-07 10:15:23 -0500

    whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
     get '/text/:term' => 'text#show'
     get '/text' => 'text#index'
-    
+
     get '/ccc/genes/:gene_entrez_id/variants' =>  'variants#entrez_gene_index'
     get '/ccc/genes/:entrez_id' =>  'genes#entrez_show'
 
