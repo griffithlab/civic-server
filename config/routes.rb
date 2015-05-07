69cc1514277f09465df831783747bc224bacee50
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-07 10:26:12 -0500

    minor route cleanup for ccc

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,10 @@ Rails.application.routes.draw do
     get '/text/:term' => 'text#show'
     get '/text' => 'text#index'
 
-    get '/ccc/genes/:gene_entrez_id/variants' =>  'variants#entrez_gene_index'
-    get '/ccc/genes/:entrez_id' =>  'genes#entrez_show'
-
+    scope 'ccc' do
+      get 'genes/:entrez_id/variants' =>  'variants#entrez_gene_index'
+      get 'genes/:entrez_id' =>  'genes#entrez_show'
+    end
 
     scope 'stats' do
       get 'current_user' => 'stats#current_user_stats'
