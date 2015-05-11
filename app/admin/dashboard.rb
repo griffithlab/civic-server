37af75b9f9a68b10fec6cc6c783412a907111544
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-11 10:17:59 -0500

    fix nesting

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -11,20 +11,20 @@ ActiveAdmin.register_page 'Dashboard' do
             end
           end
         end
-      end
-      panel "Evidence Items Requiring Action" do
-        ul do
-          EvidenceItem.includes(:creator).where(status: ['failed', 'processed']).map do |ei|
-            li link_to("#{ei.name} (by #{ei.creator.try.display_name} #{time_ago_in_words(ei.created_at)})", admin_evidence_item_path(ei))
+        panel "Evidence Items Requiring Action" do
+          ul do
+            EvidenceItem.includes(:creator).where(status: ['failed', 'processed']).map do |ei|
+              li link_to("#{ei.name} (by #{ei.creator.try.display_name} #{time_ago_in_words(ei.created_at)})", admin_evidence_item_path(ei))
+            end
           end
         end
       end
-    end
-    column do
-      panel "Recently Joined Users" do
-        ul do
-          User.order('users.created_at DESC').limit(10).map do |u|
-            li link_to("#{u.display_name} (#{time_ago_in_words(u.created_at)} ago)", admin_user_path(u))
+      column do
+        panel "Recently Joined Users" do
+          ul do
+            User.order('users.created_at DESC').limit(10).map do |u|
+              li link_to("#{u.display_name} (#{time_ago_in_words(u.created_at)} ago)", admin_user_path(u))
+            end
           end
         end
       end
