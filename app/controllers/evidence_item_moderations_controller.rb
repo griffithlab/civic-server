7e2863480f4dcf55fe74c200fbdd5a49ac434e45
Adam Coffman <acoffman@genome.wustl.edu>
2015-05-13 11:23:07 -0500

    add additional allowed params

--- app/controllers/evidence_item_moderations_controller.rb
+++ app/controllers/evidence_item_moderations_controller.rb
@@ -1,16 +1,16 @@
 class EvidenceItemModerationsController < ModerationsController
-
   private
   def moderated_object
-    EvidenceItem.find_by_id!(params[:evidence_item_id])
+    EvidenceItem.view_scope.find_by_id!(params[:evidence_item_id])
   end
 
   def moderation_params
-    params.permit(:clinical_significance, :evidence_direction, :text, :description)
+    params.permit(:clinical_significance, :evidence_direction, :text, :description, :rating,
+     :evidence_level, :variant_origin, :evidence_direction, :source)
+    #:pubmed_id
   end
 
   def presenter_class
    EvidenceItemPresenter
   end
-
 end
