ec76e4d1f7645d092341101e98c36d04c2efdf4b
Adam Coffman <acc@fastmail.com>
2015-06-02 15:24:16 -0500

    fix fabricators

--- spec/fabricators/evidence_item_fabricator.rb
+++ spec/fabricators/evidence_item_fabricator.rb
@@ -1,13 +1,13 @@
 Fabricator(:evidence_item) do
   variant
-  evidence_type
-  evidence_level
-  variant_origin
+  evidence_type { EvidenceItem.evidence_types.keys.sample }
+  evidence_level{ EvidenceItem.evidence_levels.keys.sample }
+  variant_origin { EvidenceItem.variant_origins.keys.sample }
   drugs(count: 1) { |attrs, i| Fabricate(:drug) }
   disease
   source
-  clinical_significance { sequence(:clin_sig) { |i| "Clin Sig ##{i}" } }
-  evidence_direction { sequence(:evidence_direction) { |i| "Evidence Direction ##{i}" } }
+  clinical_significance { EvidenceItem.clinical_significances.keys.sample }
+  evidence_direction { EvidenceItem.evidence_directions.keys.sample }
   description { sequence(:ei_text) { |i| "Evidence Text ##{i}" } }
   variant_hgvs { sequence(:hgvs) { |i| "Evidence HGVS ##{i}" } }
   rating { sequence(:rating) }
@@ -23,19 +23,7 @@ Fabricator(:disease) do
   name { sequence(:disease_name) { |i| "Disease name ##{i}" } }
 end
 
-Fabricator(:evidence_type) do
-  evidence_type { sequence(:evidence_type) { |i| "Evidence Type ##{i}" } }
-end
-
 Fabricator(:source) do
   pubmed_id { sequence(:pubmed) { |i| "#{i}" } }
   description { sequence(:citation) { |i| "Authors, paper ##{i}" } }
-end
-
-Fabricator(:evidence_level) do
-  level { sequence(:level) { |i| "Level ##{i}" } }
-end
-
-Fabricator(:variant_origin) do
-  origin { sequence(:variant_origin) { |i| "Origin ##{i}" } }
 end
