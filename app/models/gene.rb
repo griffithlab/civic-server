f85aa1309ec967ee96b179e8fb2a0fd7c9e81d59
Adam Coffman <acoffman@genome.wustl.edu>
2015-06-09 13:57:35 -0500

    remove default gene and variant descriptions

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -11,8 +11,6 @@ class Gene < ActiveRecord::Base
   has_and_belongs_to_many :sources
   has_and_belongs_to_many :gene_aliases
 
-  before_save :add_default_description
-
   def self.view_scope
     eager_load(:gene_aliases, :sources, :variants, variant_groups: [:variants])
   end
@@ -23,12 +21,5 @@ class Gene < ActiveRecord::Base
     .joins('LEFT OUTER JOIN variants ON variants.gene_id = genes.id')
     .joins('LEFT OUTER JOIN evidence_items ON evidence_items.variant_id = variants.id')
     .joins('LEFT OUTER JOIN diseases ON diseases.id = evidence_items.disease_id')
-  end
-
-  private
-  def add_default_description
-    if self.description.blank?
-      self.description = 'A summary for this gene has yet to be developed. Click "Edit Variant" to create one now!'
-    end
   end
 end
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -11,8 +11,6 @@ class Variant < ActiveRecord::Base
   has_many :variant_group_variants
   has_many :variant_groups, through: :variant_group_variants
 
-  before_save :add_default_description
-
   def self.index_scope
     eager_load(gene: [:gene_aliases], evidence_items: [:disease, :source])
   end
@@ -34,12 +32,5 @@ class Variant < ActiveRecord::Base
 
   def parent_subscribables
     [gene]
-  end
-
-  private
-  def add_default_description
-    if self.description.blank?
-      self.description = 'A summary for this variant has yet to be developed. Click "Edit Variant" to create one now!'
-    end
   end
 end
