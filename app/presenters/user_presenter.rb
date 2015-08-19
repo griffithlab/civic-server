f0c8dc5ec0bf3a0438b79a282e193371f81806d3
Adam Coffman <acoffman@genome.wustl.edu>
2015-08-19 14:02:09 -0500

    provide multiple size options for the avatar url

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -18,8 +18,18 @@ class UserPresenter
         username: @user.username,
         role: @user.role,
         avatar_url: gravatar_url(@user.email),
+        avatars: avatars,
         area_of_expertise: @user.area_of_expertise,
       }
     end
+  end
+
+  private
+  def avatars
+    {
+      '128x128' => gravatar_url(@user.email, size: 128),
+      '64x64' => gravatar_url(@user.email, size: 64),
+      '32x32' => gravatar_url(@user.email, size: 32)
+    }
   end
 end
