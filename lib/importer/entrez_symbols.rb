2f3049a68a9e70ceadb7ada4c302df75e70d9b99
Adam Coffman <acoffman@genome.wustl.edu>
2015-08-26 13:28:01 -0500

    make entrez gene import non-transactional

--- lib/importer/entrez_symbols.rb
+++ lib/importer/entrez_symbols.rb
@@ -1,20 +1,18 @@
 module Importer
   class EntrezSymbols
     def self.run(file_path)
-      ActiveRecord::Base.transaction do
-        Gene.auditing_enabled = false
-        file = File.new(file_path, 'r')
-        file.each_line do |row|
-          begin
-            (entrez_id, symbol) = row.split("\t").map(&:strip)[1..2]
-            next unless entrez_id.present? && symbol.present?
-            unless Gene.find_by(name: symbol, entrez_id: entrez_id)
-              gene = Gene.new(name: symbol, entrez_id: entrez_id, description: '')
-              Scrapers::MyGeneInfo.populate_gene_metadata(gene).save
-            end
-          rescue => e
-            next
+      Gene.auditing_enabled = false
+      file = File.new(file_path, 'r')
+      file.each_line do |row|
+        begin
+          (entrez_id, symbol) = row.split("\t").map(&:strip)[1..2]
+          next unless entrez_id.present? && symbol.present?
+          unless Gene.find_by(name: symbol, entrez_id: entrez_id)
+            gene = Gene.new(name: symbol, entrez_id: entrez_id, description: '')
+            Scrapers::MyGeneInfo.populate_gene_metadata(gene).save
           end
+        rescue => e
+          next
         end
       end
     end
