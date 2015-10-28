8e5a0c9f68238f79d9a38b8ed2af2a515ae12128
Adam Coffman <acoffman@genome.wustl.edu>
2015-10-28 16:18:43 -0500

    slight refactor of user presenter

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,38 +1,40 @@
 class UserPresenter
   include Gravatarify::Base
+  attr_reader :user
 
   def initialize(user)
     @user = user
   end
 
   def as_json(options = {})
-    if @user.nil?
+    if user.nil?
       {
         username: 'Admin'
       }
     else
       {
-        id: @user.id,
-        email: @user.email,
-        name: @user.name,
-        username: @user.username,
-        role: @user.role,
-        avatar_url: gravatar_url(@user.email),
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        username: user.username,
+        role: user.role,
+        avatar_url: gravatar_url(user.email),
         avatars: avatars,
-        area_of_expertise: @user.area_of_expertise,
-        orcid: @user.orcid,
-        display_name: @user.display_name
+        area_of_expertise: user.area_of_expertise,
+        orcid: user.orcid,
+        display_name: user.display_name
       }
     end
   end
 
   private
   def avatars
+    value = user.email.present? ? user.email : user.display_name
     {
-      'x128' => gravatar_url(@user.email, size: 128),
-      'x64' => gravatar_url(@user.email, size: 64),
-      'x32' => gravatar_url(@user.email, size: 32),
-      'x14' => gravatar_url(@user.email, size: 14)
+      'x128' => gravatar_url(value, size: 128),
+      'x64' => gravatar_url(value, size: 64),
+      'x32' => gravatar_url(value, size: 32),
+      'x14' => gravatar_url(value, size: 14)
     }
   end
 end
