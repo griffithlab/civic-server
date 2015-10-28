01f09e1ed4a4a5b9d8894cfc9a87f6b255ddc703
Adam Coffman <acoffman@genome.wustl.edu>
2015-10-28 16:07:10 -0500

    enable identicons for people without gravatars

--- config/initializers/gravatarify.rb
+++ config/initializers/gravatarify.rb
@@ -1,3 +1,5 @@
 Gravatarify.options[:secure] = true
 Gravatarify.options[:size] = 32
 Gravatarify.options[:filetype] = :png
+Gravatarify.options[:default] = 'identicon'
+Gravatarify.options[:rating] = 'pg'
