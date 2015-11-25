000a800f1ef39800455b39ae2eec2d3f6bec7f45
Adam Coffman <acoffman@genome.wustl.edu>
2015-11-25 12:38:22 -0600

    add index for ordering

--- app/presenters/lifecycle_presenter.rb
+++ app/presenters/lifecycle_presenter.rb
@@ -6,12 +6,15 @@ class LifecyclePresenter
   end
 
   def as_json(opts = {})
+    index = 0
     subject.lifecycle_events.each_with_object({}) do |(event_name, relation_name), h|
       if event = subject.send(relation_name)
         h[event_name] = {
+          order: index,
           timestamp: event.created_at,
           user: UserPresenter.new(event.originating_user)
         }
+        index += 1
       end
     end
   end
