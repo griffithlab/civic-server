59cd9ebe0cc9ec17a8874ebf883209f4eff690ee
Adam Coffman <acc@fastmail.com>
2015-12-21 20:55:47 -0600

    model action for extracting mentions from text

---
+++ app/models/actions/extract_mentions.rb
@@ -0,0 +1,34 @@
+module Actions
+  class ExtractMentions
+    include Actions::Transactional
+    attr_reader :mentioned_users, :invalid_mentions, :text, :regexp
+
+    def initialize(text)
+      @text = text
+      @mentioned_users = []
+      @invalid_mentions = []
+    end
+
+    def execute
+      find_matches
+      invalid_mentions.each do |mention|
+        errors << "Username #{mention} was mentioned but not found"
+      end
+    end
+
+    private
+    def find_matches
+      text.scan(self.class.regexp) do |(username)|
+        if user = User.find_by(username: username)
+          mentioned_users << user
+        else
+          invalid_mentions << username
+        end
+      end
+    end
+
+    def self.regexp
+      @regexp ||= Regexp.new(/\s@([^@\s]+)\b/)
+    end
+  end
+end
