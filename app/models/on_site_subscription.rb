c877a8a3e4be6cdafcfa35526a198b2409fedf3e
Adam Coffman <acoffman@genome.wustl.edu>
2015-12-22 10:54:37 -0600

    generate on site notifications

--- app/models/on_site_subscription.rb
+++ app/models/on_site_subscription.rb
@@ -1,4 +1,11 @@
 class OnSiteSubscription < Subscription
   def send_notification(event)
+    Notification.where(
+      type: :subscribed_event,
+      originating_user: event.originating_user,
+      notified_user: self.user,
+      event: event,
+      seen: false
+    ).first_or_create
   end
 end
