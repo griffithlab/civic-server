547ef58efa2b257bd177403c13d160dcc6a37e17
Adam Coffman <acc@fastmail.com>
2015-12-21 20:57:47 -0600

    background task for comment mention notifications

---
+++ app/jobs/notify_mentioned.rb
@@ -0,0 +1,15 @@
+class NotifyMentioned < ActiveJob::Base
+  def perform(comment, event)
+    ActiveRecord::Base.transaction do
+      result = comment.extract_mentions
+      result.mentioned_users.each do |user|
+        Notification.create(
+          type: :mention,
+          originating_user: comment.user,
+          notified_user: user,
+          event: event
+        )
+      end
+    end
+  end
+end
