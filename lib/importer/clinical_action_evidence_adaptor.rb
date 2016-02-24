cdd59810a19de4d09ad4b50f24cf1ed81066f355
Adam Coffman <acoffman@genome.wustl.edu>
2016-02-24 11:47:28 -0600

    updates to importer

--- lib/importer/clinical_action_evidence_adaptor.rb
+++ lib/importer/clinical_action_evidence_adaptor.rb
@@ -1,14 +1,41 @@
 module Importer
   class ClinicalActionEvidenceAdaptor
     def valid_row?(row)
-      row['Include?'] == '1' && required_fields.inject(true) { |val, cur| val && !row[cur].blank? }
+      if row['DOID'].blank?
+        disease_name = row['Disease']
+        if d = Disease.where('name ILIKE ?', disease_name.downcase).first
+          row['DOID'] = d.doid
+        elsif d = Disease.where('name ILIKE ?', disease_name.downcase + " cancer").first
+          row['DOID'] = d.doid
+        end
+      end
+
+      pubmed_id = row['Source']
+      if pubmed_id.present? && Source.find_by(pubmed_id: pubmed_id).blank?
+        citation = Scrapers::PubMed.get_citation_from_pubmed_id(pubmed_id)
+        if citation.present?
+          Source.create(pubmed_id: pubmed_id, description: citation)
+          row['pubmed_id'] = pubmed_id
+        end
+      end
+      result = row['Include?'] == '2' && required_fields.inject(true) { |val, cur| val && !row[cur].blank? }
+      result
     end
 
     def create_entities_for_row(row)
       variant_name = row['variant'].upcase.strip
       entrez_id = row['entrez_id']
 
-      variant = Variant.joins(:gene).find_by!(name: variant_name, 'genes.entrez_id' => entrez_id)
+      variant = Variant.joins(:gene).find_by(name: variant_name, 'genes.entrez_id' => entrez_id)
+      unless variant
+        gene = Gene.find_by(entrez_id: entrez_id)
+        unless gene
+          symbol = Scrapers::EntrezGene.get_gene_symbol_from_entrez_id(entrez_id)
+          gene = Gene.new(entrez_id: entrez_id, name: symbol, description: '')
+          Scrapers::MyGeneInfo.populate_gene_metadata(gene)
+        end
+        variant = Variant.create(gene: gene, name: variant_name, description: '')
+      end
 
       disease = EntityMaps::Disease.get_entity_from_row(row)
       source = EntityMaps::Source.get_entity_from_row(row)
