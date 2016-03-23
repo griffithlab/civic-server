b909919705891e74ac72c5919d26c1079e370ace
Adam Coffman <acoffman@genome.wustl.edu>
2016-03-23 12:09:57 -0500

    include sequence ontology url

--- app/presenters/variant_type_presenter.rb
+++ app/presenters/variant_type_presenter.rb
@@ -11,7 +11,13 @@ class VariantTypePresenter
       name: variant_type.name,
       display_name: variant_type.display_name,
       so_id: variant_type.so_id,
-      description: variant_type.description
+      description: variant_type.description,
+      url: sequence_ontology_url
     }
+  end
+
+  private
+  def sequence_ontology_url
+    "http://www.sequenceontology.org/browser/current_svn/term/#{variant_type.so_id}"
   end
 end
