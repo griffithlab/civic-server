5d6ea17856866e0d1636e64f36672bed390ea17c
Adam Coffman <acoffman@genome.wustl.edu>
2016-03-23 09:45:09 -0500

    use attr_reader instead of instance variable

--- app/presenters/variant_group_presenter.rb
+++ app/presenters/variant_group_presenter.rb
@@ -1,21 +1,33 @@
 class VariantGroupPresenter
-  def initialize(variant_group)
+  attr_reader :variant_group, :with_lifecycle
+
+  def initialize(variant_group, with_lifecycle = true)
+    @with_lifecycle = with_lifecycle
     @variant_group = variant_group
   end
 
   def as_json(options = {})
     {
-      id: @variant_group.id,
-      name: @variant_group.name,
-      description: @variant_group.description,
+      id: variant_group.id,
+      name: variant_group.name,
+      description: variant_group.description,
       variants: variants,
       type: :variant_group,
-      lifecycle_actions: LifecyclePresenter.new(@variant_group)
-    }
+    }.merge(lifecycle_actions)
   end
 
   private
   def variants
-    @variant_group.variants.uniq.map { |v| VariantPresenter.new(v) }
+    variant_group.variants.uniq.map { |v| VariantPresenter.new(v) }
+  end
+
+  def lifecycle_actions
+    if with_lifecycle
+      {
+        lifecycle_actions: LifecyclePresenter.new(variant_group)
+      }
+    else
+      {}
+    end
   end
 end
