20fdb299839b2d92237f2c4baada508d72894ae9
Adam Coffman <acoffman@genome.wustl.edu>
2016-06-08 13:35:15 -0500

    whitespace and small name fixes

--- app/presenters/variant_group_browse_row_presenter.rb
+++ app/presenters/variant_group_browse_row_presenter.rb
@@ -1,16 +1,15 @@
 class VariantGroupBrowseRowPresenter
-	def initialize(variant_group)
-		@variant_group = variant_group
-	end
+  def initialize(variant_group)
+    @variant_group = variant_group
+  end
 
-	def as_json
-		{
-			name: @variant_group.name,
-			variants: @variant_group.variants_names,
-			entrez_genes: @variant_group.entrez_names,
-			variant_count: @variant_group.variant_count,
-			evidence_item_count: @variant_group.evidence_item_count 
-			
-		}
-	end
+  def as_json
+    {
+      name: @variant_group.name,
+      variants: @variant_group.variants_names,
+      entrez_genes: @variant_group.entrez_names,
+      variant_count: @variant_group.variant_count,
+      evidence_item_count: @variant_group.evidence_item_count
+    }
+  end
 end
