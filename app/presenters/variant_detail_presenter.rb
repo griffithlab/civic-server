70f0125161e8a95329821c6f3b3d4e9b93fc740b
Adam Coffman <acoffman@genome.wustl.edu>
2016-06-09 14:39:40 -0500

    include variant aliases in variant detail view

--- app/presenters/variant_detail_presenter.rb
+++ app/presenters/variant_detail_presenter.rb
@@ -4,6 +4,7 @@ class VariantDetailPresenter < VariantIndexPresenter
       {
         evidence_items: variant.evidence_items.map { |ei| EvidenceItemIndexPresenter.new(ei) },
         variant_groups: variant.variant_groups.map { |vg| VariantGroupIndexPresenter.new(vg) },
+        variant_aliases: variant.variant_aliases.map(&:name),
         lifecycle_actions: LifecyclePresenter.new(variant),
         errors: variant.errors.to_h
       }
