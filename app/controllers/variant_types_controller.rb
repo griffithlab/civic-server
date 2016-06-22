d0670b10346b56049eb00ec914b434f1600663dc
Adam Coffman <acoffman@genome.wustl.edu>
2016-06-22 12:59:41 -0500

    variant type comparision now accepts a list

--- app/controllers/variant_types_controller.rb
+++ app/controllers/variant_types_controller.rb
@@ -18,12 +18,11 @@ class VariantTypesController < ApplicationController
   end
 
   def relationships
-    variant = Variant.eager_load(:variant_types)
-      .find(params[:variant_id])
+    existing_vts = VariantType.find(params[:existing_variant_type_ids])
 
-    new_vt = VariantType.find(params[:variant_type_id])
+    new_vt = VariantType.find(params[:new_variant_type_id])
 
-    relationships = variant.variant_types.map do |existing_vt|
+    relationships = existing_vts.map do |existing_vt|
       {
         variant_type: VariantTypePresenter.new(existing_vt),
         relationship: existing_vt.relationship_with(new_vt)
