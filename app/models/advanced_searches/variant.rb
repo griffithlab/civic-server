89dd4628f712c56fa96d036093e9dd1d6a6f8fd2
Greg Spies <gspies94@gmail.com>
2016-06-29 13:37:53 -0500

    Fix bug where reference build filter on advanced search for variants would not return results

--- app/models/advanced_searches/variant.rb
+++ app/models/advanced_searches/variant.rb
@@ -19,7 +19,7 @@ module AdvancedSearches
         'description' => default_handler.curry['variants.description'],
         'variant_group' => default_handler.curry['variant_groups.name'],
         'ensembl_version' => default_handler.curry['variants.ensembl_version'],
-        'reference_build' => default_handler.curry['variants.reference_build'], 'reference_bases' => default_handler.curry['variant.reference_bases'],
+        'reference_build' => method(:handle_reference_build), 'reference_bases' => default_handler.curry['variant.reference_bases'],
         'variant_bases' => default_handler.curry['variants.variant_bases'],
         'chromosome' => default_handler.curry['variants.chromosome'],
         'start' => default_handler.curry['variants.start'],
@@ -32,6 +32,12 @@ module AdvancedSearches
         'variant_types' => default_handler.curry['variant_types.display_name']
       }
       @handlers[field]
+    end
+    def handle_reference_build(operation_type, parameters)
+      [
+        [comparison(operation_type, 'variants.reference_build')],
+        ::Variant.reference_builds[parameters.first]
+      ]
     end
   end
 end
