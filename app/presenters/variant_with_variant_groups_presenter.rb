3b032bde17f88fb1ec8d07fa61890ff84e7b8ea5
Adam Coffman <acoffman@genome.wustl.edu>
2016-07-20 14:03:23 -0500

    typo fix

--- app/presenters/variant_with_variant_groups_presenter.rb
+++ app/presenters/variant_with_variant_groups_presenter.rb
@@ -2,7 +2,7 @@ class VariantWithVariantGroupsPresenter < VariantIndexPresenter
   def as_json(opts = {})
     super.merge(
       {
-        variant_groups: variant.variant_groups.iq.map { |vg| VariantGroupIndexPresenter.new(vg) }
+        variant_groups: variant.variant_groups.uniq.map { |vg| VariantGroupIndexPresenter.new(vg) }
       }
     )
   end
