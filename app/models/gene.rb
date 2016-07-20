e1f2561d16e6200c1fe0ea1adf1bade02327f3ca
Adam Coffman <acoffman@genome.wustl.edu>
2016-07-20 13:14:52 -0500

    user inner join rather than left join

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -21,7 +21,7 @@ class Gene < ActiveRecord::Base
   end
 
   def self.advanced_search_scope
-    index_scope
+    eager_load(:gene_aliases).joins(variants: [:evidence_items_by_status])
   end
 
   def self.datatable_scope
