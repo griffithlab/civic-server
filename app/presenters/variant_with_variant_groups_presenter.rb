9d3f34fc78af38c35597a30d5061ec86d0282946
Adam Coffman <acoffman@genome.wustl.edu>
2016-07-20 13:31:50 -0500

    use objects instead of strings

--- app/presenters/variant_with_variant_groups_presenter.rb
+++ app/presenters/variant_with_variant_groups_presenter.rb
@@ -2,7 +2,7 @@ class VariantWithVariantGroupsPresenter < VariantIndexPresenter
   def as_json(opts = {})
     super.merge(
       {
-        variant_groups: variant.variant_groups.uniq.map(&:name)
+        variant_groups: variant.variant_groups.iq.map { |vg| VariantGroupIndexPresenter.new(vg) }
       }
     )
   end
