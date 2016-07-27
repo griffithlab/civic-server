84f3abc15ea16b9cce9a15b7b811c1fe02c8cd89
Adam Coffman <acoffman@genome.wustl.edu>
2016-07-27 12:02:50 -0500

    add fields with pending changes to evidence item detail view

--- app/presenters/evidence_item_detail_presenter.rb
+++ app/presenters/evidence_item_detail_presenter.rb
@@ -3,7 +3,8 @@ class EvidenceItemDetailPresenter < EvidenceItemIndexPresenter
   def as_json(opts = {})
     super.merge({
       errors: item.errors.to_hash,
-        lifecycle_actions: LifecyclePresenter.new(item)
+      lifecycle_actions: LifecyclePresenter.new(item),
+      fields_with_pending_changes: item.fields_with_pending_changes
     })
   end
 end
