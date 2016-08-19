d2ab0411786a5f9d052074375fd516c9a657a1ff
Adam Coffman <acc@fastmail.com>
2016-08-19 12:10:42 -0500

    refactor ontology converters to remove redundant code

--- app/models/ga4gh_converters/disease.rb
+++ app/models/ga4gh_converters/disease.rb
@@ -1,27 +1,9 @@
 require 'ga4gh/genotype_phenotype'
 
 module Ga4ghConverters
-  class Disease
-    def self.from_ga4gh(json)
-      if (url = json['id']).present?
-        doid = url.split('/').last.split('_').last
-        ::Disease.find_by(doid: doid)
-      else
-        nil
-      end
-    end
-
-    def self.to_ga4gh(disease)
-      Ga4gh::OntologyTerm.new(
-        id: url_from_doid(disease.doid),
-        term: disease.name,
-        source_name: 'disease_ontology',
-        source_version: '2016-01-25'
-      )
-    end
-
-    def self.url_from_doid(doid)
-      "http://purl.obolibrary.org/obo/DOID_#{doid}"
+  class Disease < Ontology
+    def ontology_name
+      'disease_ontology'
     end
   end
 end
---
+++ app/models/ga4gh_converters/ontology.rb
@@ -0,0 +1,39 @@
+require 'ga4gh/genotype_phenotype'
+
+module Ga4ghConverters
+  class Ontology
+    def from_ga4gh(json)
+      if (url = json['id']).present?
+        ontology_id = url.split('/').last.split('_').last
+        civic_class.find_by(ontology.id_name => ontology_id)
+      else
+        nil
+      end
+    end
+
+    def to_ga4gh(civic_obj)
+      Ga4gh::OntologyTerm.new(
+        id: url_from_ontology_id(civic_obj.send(ontology.id_name)),
+        term: civic_obj.name,
+        source_name: ontology.name,
+        source_version: ontology.version
+      )
+    end
+
+    def url_from_ontology_id(id)
+      "#{ontology.permalink_format}#{id}"
+    end
+
+    def ontology
+      @ontology ||= ::Ontology.find_by(name: ontology_name)
+    end
+
+    def civic_class
+      @klass ||= "::#{ontology.civic_class}".constantize
+    end
+
+    def ontology_name
+      raise StandardError.new('Implement in subclass')
+    end
+  end
+end
