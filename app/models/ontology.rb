d7e4fd41a3bae6e85dfce637ee1621c0423542fc
Adam Coffman <acc@fastmail.com>
2016-08-19 12:09:46 -0500

    remove unused case statement

--- app/models/ontology.rb
+++ app/models/ontology.rb
@@ -1,13 +1,2 @@
 class Ontology < ActiveRecord::Base
-  def self.for_class(klass)
-    ontology_name = case klass
-    when Disease
-      'disease_ontology'
-    when VariantType
-      'Sequence Ontology'
-    else
-      nil
-    end
-    self.find_by(name: ontology_name)
-  end
 end
