57a59d6ee14fad838f07ad2bc7bb4c182a502b1c
Adam Coffman <acoffman@genome.wustl.edu>
2016-09-02 14:20:05 -0500

    populate all publication fields

--- app/jobs/fetch_source_citation.rb
+++ app/jobs/fetch_source_citation.rb
@@ -1,9 +1,5 @@
 class FetchSourceCitation < ActiveJob::Base
   def perform(source)
-    citation = Scrapers::PubMed.get_citation_from_pubmed_id(source.pubmed_id)
-    if citation.present?
-      source.description = citation
-      source.save
-    end
+    Scrapers::PubMed.populate_source_fields(source)
   end
 end
