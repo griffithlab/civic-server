f86cb56337f68938f07562de5b620f0aeffef144
Adam Coffman <acc@fastmail.com>
2016-09-20 14:14:16 -0500

    revert unintentional change

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -40,8 +40,7 @@ module Authentication
   end
 
   def current_user
-    #@current_user ||= User.view_scope.find_by(id: session[:user_id])
-    @current_user ||= User.first
+    @current_user ||= User.view_scope.find_by(id: session[:user_id])
   end
 
   def signed_in?
