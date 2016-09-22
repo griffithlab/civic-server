ef3818332a87373a3a6228bd36fa94570ce60ea7
Adam Coffman <acoffman@genome.wustl.edu>
2016-09-22 11:03:01 -0500

    include entire user object

--- app/presenters/source_suggestion_browse_row_presenter.rb
+++ app/presenters/source_suggestion_browse_row_presenter.rb
@@ -12,7 +12,7 @@ class SourceSuggestionBrowseRowPresenter
       citation: suggestion.source.description,
       pubmed_id: suggestion.source.pubmed_id,
       publication_year: suggestion.source.publication_year,
-      submitter: suggestion.user.username,
+      submitter: UserPresenter.new(suggestion.user),
       gene: suggestion.gene_name,
       variant: suggestion.variant_name,
       disease: suggestion.disease_name,
