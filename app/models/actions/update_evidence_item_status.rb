c367d73c2f8186cd6291f9817245c55f8f5d1395
Adam Coffman <acc@fastmail.com>
2016-09-23 08:46:18 -0500

    Update publication queue status on acceptance of first eid

--- app/models/actions/update_evidence_item_status.rb
+++ app/models/actions/update_evidence_item_status.rb
@@ -13,6 +13,7 @@ module Actions
     def execute
       evidence_item.lock!
       if evidence_item.status != new_status
+        update_source_status
         evidence_item.status = new_status
         evidence_item.save!
         Event.create(
@@ -24,6 +25,23 @@ module Actions
       else
         errors << "Attempted to update to status #{new_status} but it was already completed"
       end
+    end
+
+    def update_source_status
+      conditions = [
+        new_status == 'accepted',
+        evidence_item_count_for_source == 0,
+        evidence_item.source.status == 'submitted'
+      ]
+
+      if conditions.all?
+        evidence_item.source.status = 'partially curated'
+        evidence_item.source.save
+      end
+    end
+
+    def evidence_item_count_for_source
+      evidence_item.source.evidence_items.where(status: 'accepted').count
     end
   end
 end
