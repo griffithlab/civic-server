fd33250c76bbd025845fb1963eaa348f15dda480
Adam Coffman <acoffman@genome.wustl.edu>
2016-09-28 14:51:00 -0500

    add suggested change count to gene advanced search
    closes #234

--- app/models/advanced_searches/gene.rb
+++ app/models/advanced_searches/gene.rb
@@ -1,4 +1,4 @@
-module AdvancedSearches
+module AdvantcedSearches
   class Gene
     include Base
 
@@ -19,9 +19,25 @@ module AdvancedSearches
         'name' => default_handler.curry['genes.name'],
         'description' => default_handler.curry['genes.description'],
         'aliases' => default_handler.curry['gene_aliases.name'],
-        'variants' => default_handler.curry['variants.name']
+        'variants' => default_handler.curry['variants.name'],
+        'suggested_changes_count' => method(:handle_suggested_changes_count),
       }
       @handlers[field]
     end
+  end
+
+  def handle_suggested_changes_count(operation_type, parameters)
+    sanitized_status = ActiveRecord::Base.sanitize(parameters.shift)
+    having_clause = comparison(operation_type, 'COUNT(DISTINCT(suggested_changes.id))')
+
+    condition = ::Gene.select('genes.id')
+      .joins("LEFT OUTER JOIN suggested_changes ON suggested_changes.moderated_id = genes.id AND suggested_changes.status = #{sanitized_status} AND suggested_changes.moderated_type = 'Gene'")
+      .group('genes.id')
+      .having(having_clause, *parameters.map(&:to_i)).to_sql
+
+    [
+      ["genes.id IN (#{condition})"],
+      []
+    ]
   end
 end
