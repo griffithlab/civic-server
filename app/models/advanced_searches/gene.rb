45f300c4bf5cbfa233d7a7d3aeddbd7909194875
Adam Coffman <acoffman@genome.wustl.edu>
2016-09-28 15:25:56 -0500

    fix typo

--- app/models/advanced_searches/gene.rb
+++ app/models/advanced_searches/gene.rb
@@ -1,4 +1,4 @@
-module AdvantcedSearches
+module AdvancedSearches
   class Gene
     include Base
 
@@ -24,20 +24,20 @@ module AdvantcedSearches
       }
       @handlers[field]
     end
-  end
 
-  def handle_suggested_changes_count(operation_type, parameters)
-    sanitized_status = ActiveRecord::Base.sanitize(parameters.shift)
-    having_clause = comparison(operation_type, 'COUNT(DISTINCT(suggested_changes.id))')
+    def handle_suggested_changes_count(operation_type, parameters)
+      sanitized_status = ActiveRecord::Base.sanitize(parameters.shift)
+      having_clause = comparison(operation_type, 'COUNT(DISTINCT(suggested_changes.id))')
 
-    condition = ::Gene.select('genes.id')
-      .joins("LEFT OUTER JOIN suggested_changes ON suggested_changes.moderated_id = genes.id AND suggested_changes.status = #{sanitized_status} AND suggested_changes.moderated_type = 'Gene'")
-      .group('genes.id')
-      .having(having_clause, *parameters.map(&:to_i)).to_sql
+      condition = ::Gene.select('genes.id')
+        .joins("LEFT OUTER JOIN suggested_changes ON suggested_changes.moderated_id = genes.id AND suggested_changes.status = #{sanitized_status} AND suggested_changes.moderated_type = 'Gene'")
+        .group('genes.id')
+        .having(having_clause, *parameters.map(&:to_i)).to_sql
 
-    [
-      ["genes.id IN (#{condition})"],
-      []
-    ]
+      [
+        ["genes.id IN (#{condition})"],
+        []
+      ]
+    end
   end
 end
