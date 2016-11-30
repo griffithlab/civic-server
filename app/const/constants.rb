f05378f7140a19bbcb649cb2a02ce361c0a850da
Adam Coffman <acoffman@genome.wustl.edu>
2016-11-30 10:21:38 +0100

    use epoch as default all_time value

--- app/const/constants.rb
+++ app/const/constants.rb
@@ -4,7 +4,7 @@ module Constants
     'this_week' => 1.week.ago,
     'this_month' => 1.month.ago,
     'this_year' => 1.year.ago,
-    'all_time' => Time.new('0001-01-01')
+    'all_time' => Time.new('1970-01-01 00:00:00')
   }
 
   DISPLAY_NAME_QUERY = 'users.username ILIKE :query OR users.email ILIKE :query OR users.name ILIKE :query'
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 
   def self.datatable_scope
     joins('LEFT OUTER JOIN events ON events.originating_user_id = users.id')
-      .includes(domain_expert_tags: [:domain_of_expertise])
+      .includes(:badges, domain_expert_tags: [:domain_of_expertise])
   end
 
   def self.index_scope
