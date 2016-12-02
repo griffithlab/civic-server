86ab6b0045c3ee20f56c0eb16c1134e92d9be318
Zachary Skidmore <zlskidmore@gmail.com>
2016-12-02 13:15:27 +0000

    added preliminary badges

--- app/models/badges/definitions.rb
+++ app/models/badges/definitions.rb
@@ -5,10 +5,70 @@ module Badges
     def self.badges
       #Define your badges here
       [
+        # badges for comments
         {
-          name: 'Commenter',
+          name: 'Bronze Commenter',
           tier: 'bronze',
           description: 'Made their first comment.'
+        },
+        {
+          name: 'Silver Commenter',
+          tier: 'silver',
+          description: 'Reached 10 comments.'
+        },
+        {
+          name: 'Gold Commenter',
+          tier: 'gold',
+          description: 'Reached 100 comments.'
+        },
+        {
+          name: 'Platinum Commenter',
+          tier: 'platinum',
+          description: 'reached 1000 comments.'
+        },
+        # badges for evidence items 
+        {
+          name: 'Bronze Contributor',
+          tier: 'bronze',
+          description: 'Their first evidence item was accepted'
+        },
+        {
+          name: 'Silver Contributor',
+          tier: 'silver',
+          description: 'reached 10 accepted evidence items'
+        },
+        {
+          name: 'Gold Contributor',
+          tier: 'gold',
+          description: 'reached 100 accepted evidence items'
+        },
+        {
+          name: 'Platinum Contributor',
+          tier: 'platinum',
+          description: 'reached 1000 accepted evidence items'
+        },
+        # badges for attending NKI 2016 events
+        {
+          name: 'NKI 2016 hackathon',
+          tier: 'special',
+          description: 'Attended the NKI Hackathon in Amsterdam, NL',
+          additional_fields: {
+           x128: '/public/badges/badge_hackathon_NKI2016_128.png',
+           x64: '/public/badges/badge_hackathon_NKI2016_64.png',
+           x32: '/public/badges/badge_hackathon_NKI2016_132.png',
+           x15: '/public/badges/badge_hackathon_NKI2016_15.png'
+          }
+        },
+        {
+          name: 'NKI 2016 jamboree',
+          tier: 'special',
+          description: 'Attended the NKI Jamboree in Amsterdam, NL',
+          additional_fields: {
+           x128: '/public/badges/badge_jamoree_NKI2016_128.png',
+           x64: '/public/badges/badge_jamboree_NKI2016_64.png',
+           x32: '/public/badges/badge_jamboree_NKI2016_132.png',
+           x15: '/public/badges/badge_jamboree_NKI2016_15.png'
+          }
         }
       ]
     end
