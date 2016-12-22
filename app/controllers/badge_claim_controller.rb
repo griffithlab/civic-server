411c998a2cf7de88814805170143450d4a81449f
Adam Coffman <acoffman@acoffman-macbook-pro.local>
2016-12-22 10:31:50 -0600

    Add endpoint for recently awarded badges

--- app/controllers/badge_claim_controller.rb
+++ app/controllers/badge_claim_controller.rb
@@ -1,4 +1,19 @@
 class BadgeClaimController < ApplicationController
+  actions_without_auth :index
+
+  def index
+    badge_claims = BadgeClaim.includes(:badge, :user)
+      .order('badge_claims.created_at desc')
+      .page(params[:page].to_i)
+      .per(params[:count].to_i)
+
+    render json: PaginatedCollectionPresenter.new(
+      badge_claims,
+      request,
+      BadgeClaimPresenter,
+      PaginationPresenter
+    )
+  end
 
   def redeem
     claim = BadgeClaim.find_by_redemption_code(params[:claim_id])
@@ -10,4 +25,5 @@ class BadgeClaimController < ApplicationController
       render json: { errors: result.errors }, status: :bad_request
     end
   end
+
 end
