a88147b9055dfbddcb28cd1efe7b2823a206592f
Adam Coffman <acc@fastmail.com>
2017-01-11 15:53:25 -0600

    additional dashboard metrics

--- app/models/overview_dashboard.rb
+++ app/models/overview_dashboard.rb
@@ -15,32 +15,41 @@ class OverviewDashboard
       'counts_by_clinical_significance' => ->() { EvidenceItem.count_by_clinical_significance },
       'counts_by_rating' => ->() { EvidenceItem.group(:rating).count },
       'counts_by_status' => ->() { EvidenceItem.group(:status).count },
-      'top_journals_with_levels' => method(:top_journals_with_levels),
-      'top_journals_with_types' => method(:top_journals_with_types),
+      'top_journals_with_levels' => ->() { count_eids_by_field(top_journals, :journal, :evidence_level) },
+      'top_journals_with_types' => ->() { count_eids_by_field(top_journals, :journal, :evidence_type) },
       'count_by_source_publication_year' => method(:count_by_publication_year),
+      'top_diseases_with_levels' => ->() { count_eids_by_field(top_diseases, :display_name, :evidence_level) },
+      'top_diseases_with_types' => ->() { count_eids_by_field(top_diseases, :display_name, :evidence_type) },
+      'top_drugs_with_levels' => ->() { count_eids_by_field(top_drugs, :name, :evidence_level) },
+      'top_drugs_with_clinical_significance' => ->() { count_eids_by_field(top_drugs, :name, :clinical_significance) },
     }
   end
 
-  def top_journals_with_levels
-    top_journals.each_with_object({}) do |source, h|
-      counts_by_level = {}
-      EvidenceItem.where(id: source.eids)
-        .group(:evidence_level)
+  def count_eids_by_field(objs, key, enumerated_field)
+    objs.each_with_object({}) do |entity, h|
+      counts = {}
+      EvidenceItem.where(id: entity.eids)
+        .group(enumerated_field)
         .count
-        .each { |(k,v)| next if k.nil?; counts_by_level[EvidenceItem.evidence_levels.key(k).downcase] = v }
-      h[source.journal] = counts_by_level
+        .each { |(k,v)| next if k.blank?; counts[EvidenceItem.send(enumerated_field.to_s.pluralize).key(k).downcase] = v }
+      h[entity.send(key)] = counts
     end
   end
 
-  def top_journals_with_types
-    top_journals.each_with_object({}) do |source, h|
-      counts_by_type = {}
-      EvidenceItem.where(id: source.eids)
-        .group(:evidence_type)
-        .count
-        .each { |(k,v)| next if k.nil?; counts_by_type[EvidenceItem.evidence_types.key(k).downcase] = v }
-      h[source.journal] = counts_by_type
-    end
+  def top_drugs
+    @top_drugs ||= Drug.joins(:evidence_items)
+      .group('drugs.name')
+      .select('drugs.name, array_agg(distinct(evidence_items.id)) as eids')
+      .order('count(distinct(evidence_items.id)) desc')
+      .limit(25)
+  end
+
+  def top_diseases
+    @top_diseases ||= Disease.joins(:evidence_items)
+      .group('diseases.display_name')
+      .select('diseases.display_name, array_agg(evidence_items.id) as eids')
+      .order('count(distinct(evidence_items.id)) desc')
+      .limit(25)
   end
 
   def top_journals
