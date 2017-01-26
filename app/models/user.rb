f384b25bedf78f1f589996238c106b50255fbfb5
Adam Coffman <acc@fastmail.com>
2017-01-26 14:22:13 -0600

    load badge awards on community page

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   has_many :domain_expert_tags
   has_many :badge_awards
   has_many :badge_claims
+  belongs_to :organization
 
   enum area_of_expertise: ['Patient Advocate', 'Clinical Scientist', 'Research Scientist']
   enum role: ['curator', 'editor', 'admin']
@@ -22,7 +23,7 @@ class User < ActiveRecord::Base
 
   def self.datatable_scope
     joins('LEFT OUTER JOIN events ON events.originating_user_id = users.id')
-      .includes(:badges, domain_expert_tags: [:domain_of_expertise])
+      .includes(:badge_awards, domain_expert_tags: [:domain_of_expertise])
   end
 
   def self.index_scope
