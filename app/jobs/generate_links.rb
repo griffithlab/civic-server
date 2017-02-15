34120bc92a667e78f9305578c6642cb84bfb3173
Aaron Graubert <captianjroot@live.com>
2017-02-15 14:13:06 -0600

    Reordered link generation
    Should minimize duplicated content across the links, while still covering all entities

--- app/jobs/generate_links.rb
+++ app/jobs/generate_links.rb
@@ -6,27 +6,27 @@ class GenerateLinks < ActiveJob::Base
           f.puts line
         end
       end
-      Gene.pluck(:id).each do |id|
-        f.puts "    <a href=\"/#/events/genes/%{ID}/summary\">Gene %{ID}</a><br>" % {
-          :ID => id
-        }
-      end
-      f.flush
-      Variant.pluck(:id, :gene_id).each do |var|
-        f.puts "    <a href=\"/#/events/genes/%{GID}/summary/variants/%{VID}/summary\">Variant %{VID}</a></br>" % {
-          :VID => var[0],
-          :GID => var[1]
-        }
-      end
-      f.flush
+      #Reordered generation, since evidence items are more important
+      #Ever evidence item will always have a variant and gene, so all of these
+      #links will always have a full page of gene->variant->evidence
       EvidenceItem.where("status not like 'rejected'").joins("LEFT JOIN variants ON variants.id = evidence_items.variant_id").pluck(:id, :variant_id, :gene_id).each do |var|
-        f.puts "    <a href=\"/#/events/genes/%{GID}/summary/variants/%{VID}/summary/evidence/%{EID}/summary\">Evidence %{EID}</a></br>" % {
+        f.puts "    <a href=\"/events/genes/%{GID}/summary/variants/%{VID}/summary/evidence/%{EID}/summary\">Evidence %{EID}</a></br>" % {
           :EID => var[0],
           :VID => var[1],
           :GID => var[2]
         }
       end
       f.flush
+      #skip variants, since the above items will have covered all of them
+      #Variants without evidence are not accessible in CIViC
+      #Lastly, select all genes for which there are no variants/evidence
+      Gene.joins("LEFT JOIN (SELECT variants.* from variants INNER JOIN evidence_items ON evidence_items.variant_id = variants.id) AS variants ON variants.gene_id = genes.id").where("variants.gene_id IS NULL").pluck(:id).each do |id|
+        f.puts "    <a href=\"/events/genes/%{ID}/summary\">Gene %{ID}</a><br>" % {
+          :ID => id
+        }
+      end
+      f.flush
+      #These two queries should form the minimal set of links to cover all content
       File.open("text/links_footer.html", 'r') do |reader|
         while line = reader.gets
           f.puts line
