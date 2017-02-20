8bc6a4ab8c7588e139e93c07a292cb256518b23e
Adam Coffman <acc@fastmail.com>
2017-02-20 14:15:25 -0600

    support both command formats for pg_dump

--- lib/database/dump.rb
+++ lib/database/dump.rb
@@ -3,5 +3,9 @@ module Database
     def self.command(file = Rails.configuration.data_dump_path)
       "pg_dump -E UTF8 -a -f #{file} -E UTF8"
     end
+
+    def self.prepare_command_with_db_credentials(command)
+      super(command, no_database_flag: true)
+    end
   end
 end
