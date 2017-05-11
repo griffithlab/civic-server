cd94fb67fcb3bb54bed43b56fa419ad2ea00f08c
Adam Coffman <acc@fastmail.com>
2017-05-11 11:52:22 -0500

    minor change to let rack::rewrite do its thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.serve_static_files = false
+  #not generally needed, just want the middleware active
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
