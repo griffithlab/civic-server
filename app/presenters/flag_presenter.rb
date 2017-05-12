fc025585967df62480c5b9907e11125ea22dc672
Adam Coffman <acc@fastmail.com>
2017-05-12 15:45:27 -0500

    include flaggable type and id on presenter

--- app/presenters/flag_presenter.rb
+++ app/presenters/flag_presenter.rb
@@ -14,7 +14,9 @@ class FlagPresenter
       comments: flag.comments.map { |c| CommentPresenter.new(c) },
       created_at: flag.created_at,
       updated_at: flag.updated_at,
-      state_params: flag.state_params
+      state_params: flag.state_params,
+      flaggable_type: flag.flaggable_type,
+      flaggable_id: flag.flaggable_id
     }
   end
 end
