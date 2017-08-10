26f4caaa4710d382d57e37d28acf5c235399436b
Adam Coffman <acc@fastmail.com>
2017-08-09 23:28:14 -0500

    small cap updates

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 server "ec2-18-220-31-206.us-east-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
 
+set :rbenv_ruby, '2.3.4'
+
 set :ssh_options, {
   keys: %w(/Users/acoffman/.ssh/acoffman-rails-testing.pem),
   forward_agent: false,
