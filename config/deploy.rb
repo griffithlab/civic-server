d7659e9b2531fb6f88f3b6694dd833cd2967a032
Adam Coffman <acc@fastmail.com>
2017-08-09 23:17:06 -0500

    migrate db as app role

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,4 @@ set :rbenv_type, :user
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
+set :migration_role, :app
