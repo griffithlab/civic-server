d7b08ea2bf5d22c4428f7e945c49113d4634dcb2
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-08-29 12:43:47 -0500

    Add endpoint for all assertions related to a variant

--- app/controllers/assertions_controller.rb
+++ app/controllers/assertions_controller.rb
@@ -1,5 +1,5 @@
 class AssertionsController < ApplicationController
-  actions_without_auth :index, :show
+  actions_without_auth :index, :show, :variant_index
 
   def index
     assertions = Assertion.index_scope
@@ -19,5 +19,22 @@ class AssertionsController < ApplicationController
   def show
     assertion = Assertion.view_scope.find_by!(id: params[:id])
     render json: AssertionDetailPresenter.new(assertion)
+  end
+
+  def variant_index
+    assertions = Assertion.index_scope
+      .order('assertions.id asc')
+      .page(params[:page].to_i)
+      .per(params[:count].to_i)
+      .joins(evidence_items: [:variant])
+      .where(variants: { id: params[:variant_id] })
+      .uniq
+
+    render json: PaginatedCollectionPresenter.new(
+      assertions,
+      request,
+      AssertionIndexPresenter,
+      PaginationPresenter
+    )
   end
 end
