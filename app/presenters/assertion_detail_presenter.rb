c3f11cc954d964a813d749402f7fcbc5a2af57c8
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-10-06 16:30:33 -0500

    Use correct drug presenter

--- app/presenters/assertion_detail_presenter.rb
+++ app/presenters/assertion_detail_presenter.rb
@@ -12,7 +12,7 @@ class AssertionDetailPresenter < AssertionIndexPresenter
         clinical_significance: assertion.clinical_significance,
         evidence_items: assertion.evidence_items.map { |ei| EvidenceItemIndexPresenter.new(ei) },
         acmg_codes: assertion.acmg_codes.map { |ac| AcmgCodeIndexPresenter.new(ac) },
-        drugs: assertion.drugs.map { |d| DrugIndexPresenter.new(d) },
+        drugs: assertion.drugs.map { |d| DrugPresenter.new(d) },
         drug_interaction_type: assertion.drug_interaction_type,
         fda_regulatory_approval: assertion.fda_regulatory_approval,
         fda_companion_test: assertion.fda_companion_test,
