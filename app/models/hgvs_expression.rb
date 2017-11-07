bf6c0d0aad11a415173d456e704ba15540b21c49
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-11-07 14:38:04 -0600

    Add proxy for allele registry

--- app/models/hgvs_expression.rb
+++ app/models/hgvs_expression.rb
@@ -6,6 +6,14 @@ class HgvsExpression < ActiveRecord::Base
   end
 
   def self.my_gene_info_hgvs(variant)
+    hgvs(variant, 'my_gene_info')
+  end
+
+  def self.allele_registry_hgvs(variant)
+    hgvs(variant, 'allele_registry')
+  end
+
+  def self.hgvs(variant, type)
     valid_for_query =[
       variant.chromosome.nil?,
       variant.chromosome2.present?,
@@ -14,7 +22,11 @@ class HgvsExpression < ActiveRecord::Base
     ].none?
 
     if valid_for_query
-      chromosome = "chr#{variant.chromosome}"
+      if type == 'my_gene_info'
+        chromosome = "chr#{variant.chromosome}"
+      elsif type == 'allele_registry'
+        chromosome = refseq_sequence(variant.chromosome)
+      end
       base_hgvs = "#{chromosome}:g.#{variant.start}"
       case variant_type(variant)
       when :deletion
@@ -55,5 +67,35 @@ class HgvsExpression < ActiveRecord::Base
     else
       nil
     end
+  end
+
+  def self.refseq_sequence(chromosome)
+    sequences = {
+      '1' =>  'NC_000001.10',
+      '2' =>  'NC_000002.11',
+      '3' =>  'NC_000003.11',
+      '4' =>  'NC_000004.11',
+      '5' =>  'NC_000005.9',
+      '6' =>  'NC_000006.11',
+      '7' =>  'NC_000007.13',
+      '8' =>  'NC_000008.10',
+      '9' =>  'NC_000009.11',
+      '10' => 'NC_000010.10',
+      '11' => 'NC_000011.9',
+      '12' => 'NC_000012.11',
+      '13' => 'NC_000013.10',
+      '14' => 'NC_000014.8',
+      '15' => 'NC_000015.9',
+      '16' => 'NC_000016.9',
+      '17' => 'NC_000017.10',
+      '18' => 'NC_000018.9',
+      '19' => 'NC_000019.9',
+      '20' => 'NC_000020.10',
+      '21' => 'NC_000021.8',
+      '22' => 'NC_000022.10',
+      'X' =>  'NC_000023.10',
+      'Y' =>  'NC_000024.9',
+    }
+    sequences[chromosome]
   end
 end
