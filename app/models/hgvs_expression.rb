59872ad0e5e11ed567a7e91c51246e97338e4df1
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-11-10 08:22:41 -0600

    Support both build 37 and build 38

--- app/models/hgvs_expression.rb
+++ app/models/hgvs_expression.rb
@@ -25,7 +25,13 @@ class HgvsExpression < ActiveRecord::Base
       if type == 'my_gene_info'
         chromosome = "chr#{variant.chromosome}"
       elsif type == 'allele_registry'
-        chromosome = refseq_sequence(variant.chromosome)
+        if variant.reference_build == 'GRCh38'
+          chromosome = refseq_sequence_b38(variant.chromosome)
+        elsif variant.reference_build == 'GRCh37'
+          chromosome = refseq_sequence_b37(variant.chromosome)
+        else
+          return
+        end
       end
       base_hgvs = "#{chromosome}:g.#{variant.start}"
       case variant_type(variant)
@@ -69,7 +75,38 @@ class HgvsExpression < ActiveRecord::Base
     end
   end
 
-  def self.refseq_sequence(chromosome)
+
+  def self.refseq_sequence_b38(chromosome)
+    sequences = {
+      '1' => 'NC_000001.11',
+      '2' => 'NC_000002.12',
+      '3' => 'NC_000003.12',
+      '4' => 'NC_000004.12',
+      '5' => 'NC_000005.10',
+      '6' => 'NC_000006.12',
+      '7' => 'NC_000007.14',
+      '8' => 'NC_000008.11',
+      '9' => 'NC_000009.12',
+      '10' => 'NC_000010.11',
+      '11' => 'NC_000011.10',
+      '12' => 'NC_000012.12',
+      '13' => 'NC_000013.11',
+      '14' => 'NC_000014.9',
+      '15' => 'NC_000015.10',
+      '16' => 'NC_000016.10',
+      '17' => 'NC_000017.11',
+      '18' => 'NC_000018.10',
+      '19' => 'NC_000019.10',
+      '20' => 'NC_000020.11',
+      '21' => 'NC_000021.9',
+      '22' => 'NC_000022.11',
+      'X' => 'NC_000023.11',
+      'Y' => 'NC_000024.10',
+    }
+    sequences[chromosome]
+  end
+
+  def self.refseq_sequence_b37(chromosome)
     sequences = {
       '1' =>  'NC_000001.10',
       '2' =>  'NC_000002.11',
