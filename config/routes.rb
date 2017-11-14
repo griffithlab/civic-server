80d6470a67174cfdc6707f114817088f9aceec69
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-11-14 09:45:46 -0600

    Revert "Glob gene id in route to allow for slashes in gene name"
    This reverts commit e5010bd9030eaa08c648d5e90ba63f28083c44bb.

--- config/routes.rb
+++ config/routes.rb
@@ -78,8 +78,7 @@ Rails.application.routes.draw do
       concerns :commentable, controller: 'variant_group_comments'
     end
 
-    resources 'genes', except: [:get, :edit, :new] do
-      get '*id' => 'genes#show', on: :collection
+    resources 'genes', except: [:edit, :new] do
       get 'variants_status' => 'variants#variant_navigation'
       get 'mygene_info_proxy' => 'genes#mygene_info_proxy'
       get 'variants' => 'variants#gene_index'
