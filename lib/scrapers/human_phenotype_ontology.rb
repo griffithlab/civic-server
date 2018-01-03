4dcbda1accab5d428c76f6f6060800a93513b9c9
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-03 10:03:11 -0600

    Add method to scrape the full HPO

--- lib/scrapers/human_phenotype_ontology.rb
+++ lib/scrapers/human_phenotype_ontology.rb
@@ -7,6 +7,23 @@ module Scrapers
       ''
     end
 
+    def self.scrape_all()
+      resp = Util.make_get_request(url())
+      xml = Nokogiri::XML(resp)
+      i = 0
+      xml.xpath("//owl:Class[@rdf:about]").each do |row|
+        about = row.attributes['about'].value
+        if about.starts_with?('http://purl.obolibrary.org/obo/HP_')
+          hpo_id = about[/http:\/\/purl\.obolibrary\.org\/obo\/(HP_[0-9]*)/, 1]
+          hpo_id = hpo_id.sub('_', ':')
+          name = row.at_xpath('rdfs:label').text
+          Phenotype.create(hpo_id: hpo_id, hpo_class: name)
+          i = i+  1
+        end
+      end
+      puts(i)
+    end
+
     private
     def self.url
       "https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/hp.owl"
