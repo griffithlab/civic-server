2584a6b0506c5ccfb3b3db6b4da37c7c6dce93b2
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-09 12:28:12 -0600

    Show more information for gene local suggestions

--- app/models/gene_name_suggestion.rb
+++ app/models/gene_name_suggestion.rb
@@ -1,7 +1,7 @@
 class GeneNameSuggestion
   def self.get_local_suggestions(name)
-    Gene.where('name ILIKE :name', name: "%#{name}%")
+    genes = Gene.where('name ILIKE :name', name: "%#{name}%")
       .limit(12)
-      .pluck(:name)
+    genes.map { |g| { id: g.id, name: g.name, entrez_id: g.entrez_id, aliases: g.gene_aliases.map(&:name)} }
   end
 end
