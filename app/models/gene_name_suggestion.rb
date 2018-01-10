da66da04283ed01ca75304af78bf96d902db0742
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-10 12:39:23 -0600

    Filter out genes without variants

--- app/models/gene_name_suggestion.rb
+++ app/models/gene_name_suggestion.rb
@@ -1,6 +1,7 @@
 class GeneNameSuggestion
   def self.get_local_suggestions(name)
-    genes = Gene.where('name ILIKE :name', name: "%#{name}%")
+    genes = Gene.eager_load(:variants).where('genes.name ILIKE :name', name: "%#{name}%")
+      .where('variants.gene_id IS NOT NULL')
       .limit(12)
     genes.map { |g| { id: g.id, name: g.name, entrez_id: g.entrez_id, aliases: g.gene_aliases.map(&:name)} }
   end
