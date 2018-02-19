91688656a5fc80cf696b7a28c392400a35980965
Adam Coffman <acc@fastmail.com>
2018-02-19 16:45:22 -0600

    disable asset compilation in cap

--- Capfile
+++ Capfile
@@ -4,7 +4,7 @@ require "capistrano/scm/git"
 install_plugin Capistrano::SCM::Git
 require "capistrano/rbenv"
 require "capistrano/bundler"
-require 'capistrano/rails'
+require 'capistrano/rails/migrations'
 require "capistrano/passenger"
 require 'capistrano/delayed_job'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,6 @@ set :rbenv_type, :user
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/downloads')
 
+
+set :assets_roles, []
 set :migration_role, :web
