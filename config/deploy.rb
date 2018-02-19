4115e4c90d00dc4c06820615a86a711d6e29bd10
Adam Coffman <acc@fastmail.com>
2018-02-19 16:22:23 -0600

    additional linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,6 @@ set :deploy_to, '/var/www/civic/'
 set :rbenv_type, :user
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads', 'public/downloads')
 
 set :migration_role, :web
