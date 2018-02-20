9c70be2a6faddf90086bc935a15d26b9cd0fb8bd
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-20 09:21:37 -0600

    Update staging.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
-server "ec2-18-220-31-206.us-east-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
+server "ec2-34-217-230-24.us-west-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
 
 set :rbenv_ruby, '2.3.4'
 
 set :ssh_options, {
-  keys: %w(/Users/acoffman/.ssh/civic-staging.pem),
+  keys: ENV['CIVIC_STAGING_KEY'],
   forward_agent: false,
   auth_methods: %w(publickey)
 }
