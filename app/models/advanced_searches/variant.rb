2cb3708f0c25641efa146ed6c525fa6cd247409b
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-26 11:55:50 -0600

    Support advanced search by pipeline type

--- app/models/advanced_searches/variant.rb
+++ app/models/advanced_searches/variant.rb
@@ -37,6 +37,7 @@ module AdvancedSearches
         'suggested_changes_count' => method(:handle_suggested_changes_count),
         'evidence_item_count' => method(:handle_evidence_item_count),
         'civic_actionability_score' => default_handler.curry['variants.civic_actionability_score'],
+        'pipeline_type' => default_handler.curry['pipeline_types.name']
       }
       @handlers[field]
     end
