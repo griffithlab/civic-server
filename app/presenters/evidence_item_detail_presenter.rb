1a84a18803d78354808d881f95b1bd28ce3cc8ec
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-03-13 14:11:10 -0500

    Add assertions to evidence item detail presenter

--- app/presenters/evidence_item_detail_presenter.rb
+++ app/presenters/evidence_item_detail_presenter.rb
@@ -2,6 +2,7 @@ class EvidenceItemDetailPresenter < EvidenceItemIndexPresenter
 
   def as_json(opts = {})
     super.merge({
+      assertions: item.assertions.map{ |a| AssertionIndexPresenter.new(a) },
       errors: item.errors.to_hash,
       lifecycle_actions: LifecyclePresenter.new(item),
       fields_with_pending_changes: item.fields_with_pending_changes,
