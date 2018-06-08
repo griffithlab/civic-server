477f799813d00afcc6f53a971080ee92c76c960c
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-06-08 11:14:46 -0500

    Add more tests for the EvidenceItemsController

--- spec/controllers/evidence_item_spec.rb
+++ spec/controllers/evidence_item_spec.rb
@@ -4,7 +4,7 @@ describe EvidenceItemsController do
   it 'should index' do
     evidence_item = Fabricate(:evidence_item, status: 'accepted')
 
-    get :index
+    get :index, status: 'accepted'
 
     result = JSON.parse(response.body)
     expect(result['records'].length).to eq 1
@@ -34,6 +34,37 @@ describe EvidenceItemsController do
     expect(result['records'][0]['name']).to eq evidence_item.name
   end
 
+  it 'should accept' do
+    evidence_item = Fabricate(:evidence_item, status: 'submitted')
+    user = Fabricate(:user, role: :admin)
+    controller.sign_in(user)
+
+    post :accept, evidence_item_id: evidence_item.id
+    evidence_item.reload
+
+    expect(evidence_item.status).to eq 'accepted'
+  end
+
+  it 'should reject' do
+    evidence_item = Fabricate(:evidence_item, status: 'submitted')
+    user = Fabricate(:user, role: :admin)
+    controller.sign_in(user)
+
+    post :reject, evidence_item_id: evidence_item.id
+    evidence_item.reload
+
+    expect(evidence_item.status).to eq 'rejected'
+  end
+
+  it 'should show' do
+    evidence_item = Fabricate(:evidence_item, status: 'accepted')
+
+    get :show, id: evidence_item
+
+    result = JSON.parse(response.body)
+    expect(result['name']).to eq evidence_item.name
+  end
+
   it 'should destroy' do
     evidence_item = Fabricate(:evidence_item)
     user = Fabricate(:user, role: :admin)
@@ -42,5 +73,15 @@ describe EvidenceItemsController do
     expect(EvidenceItem.count).to eq 1
     delete :destroy, id: evidence_item
     expect(EvidenceItem.count).to eq 0
+  end
+
+  it 'should datatable' do
+    evidence_item = Fabricate(:evidence_item, status: 'accepted')
+
+    get :datatable
+
+    result = JSON.parse(response.body)
+    expect(result['result'].length).to eq 1
+    expect(result['result'][0]['id']).to eq evidence_item.id
   end
 end
