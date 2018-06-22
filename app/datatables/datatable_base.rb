8a8f92ddbb6ac389d48df0addbad0b3540851648
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-06-22 13:35:06 -0500

    Handle new sort_priority parameter

--- app/datatables/datatable_base.rb
+++ app/datatables/datatable_base.rb
@@ -65,7 +65,8 @@ class DatatableBase
 
   def order(objects)
     if sort_params = params['sorting']
-      sort_params.inject(objects) do |o, (col, direction)|
+      params['sort_priority'].split(',').inject(objects) do |o, (col)|
+        direction = params['sorting'][col]
         if actual_col = order_column(col)
           o.order("#{actual_col} #{sort_direction(direction)} NULLS LAST")
         else
