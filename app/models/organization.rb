058ce4bcf4b38a04d54ff4de59b12455ac6e07d0
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-10-29 12:54:12 -0500

    Allow organizations to have parent organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,7 @@
 class Organization < ActiveRecord::Base
   has_many :users
+  belongs_to :parent, :class_name => 'Organization'
+  has_many :groups, :class_name => 'Organization', :foreign_key => 'parent_id'
 
   has_attached_file :profile_image,
     styles: { x256: '256x256', x128: '128x128', x64: '64x64', x32: '32x32', x14: '14x14' }
@@ -15,11 +17,19 @@ class Organization < ActiveRecord::Base
 
   def stats_hash
     (Hash.new { |h, k| h[k] = 0 }).tap do |accum|
-      users.each do |u|
+      all_users.each do |u|
         u.stats_hash.each do |k, v|
           accum[k] += v
         end
       end
     end
+  end
+
+  def all_users
+    users = self.users
+    self.groups.each_with_object(users) do |g, u|
+      u << g.users
+    end
+    return users
   end
 end
