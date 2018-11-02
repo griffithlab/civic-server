4c52b4212b6c03ccc75ae931ce8e9615babc5147
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-11-02 13:08:21 -0500

    Add assertion count to Variant browse table

--- app/datatables/variant_browse_table.rb
+++ app/datatables/variant_browse_table.rb
@@ -26,7 +26,7 @@ class VariantBrowseTable < DatatableBase
   end
 
   def select_query
-    initial_scope.select('variants.id, variants.name, variants.civic_actionability_score, array_agg(distinct(diseases.name) order by diseases.name asc) as disease_names, max(genes.id) as gene_id, max(genes.entrez_id) as entrez_id, max(genes.name) as entrez_name, count(distinct(evidence_items.id)) as evidence_item_count, array_agg(distinct(drugs.name) order by drugs.name) as drug_names')
+    initial_scope.select('variants.id, variants.name, variants.civic_actionability_score, array_agg(distinct(diseases.name) order by diseases.name asc) as disease_names, max(genes.id) as gene_id, max(genes.entrez_id) as entrez_id, max(genes.name) as entrez_name, count(distinct(evidence_items.id)) as evidence_item_count, array_agg(distinct(drugs.name) order by drugs.name) as drug_names, count(distinct(assertions.id)) as assertion_count')
       .where("evidence_items.status != 'rejected'")
       .group('variants.id, variants.name')
       .having('count(distinct(evidence_items.id)) > 0')
