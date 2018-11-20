72b1abf0a1bcf658bd753c508e84097f34678d36
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-11-20 16:25:11 -0600

    Add suggested changes advanced search functionality

---
+++ app/models/advanced_searches/suggested_change.rb
@@ -0,0 +1,26 @@
+module AdvancedSearches
+  class SuggestedChange
+    include Base
+
+    def initialize(params)
+      @params = params
+      @presentation_class = SuggestedChangePresenter
+    end
+
+    def model_class
+      ::SuggestedChange
+    end
+
+    private
+    def handler_for_field(field)
+      default_handler = method(:default_handler).to_proc
+      @handlers ||= {
+        'moderated_type' => default_handler.curry['suggested_changes.moderated_type'],
+        'moderated_id' => default_handler.curry['suggested_changes.moderated_id'],
+        'submitter' => default_handler.curry[['users.email', 'users.name', 'users.username']],
+        'submitter_id' => default_handler.curry['users.id'],
+      }
+      @handlers[field]
+    end
+  end
+end
