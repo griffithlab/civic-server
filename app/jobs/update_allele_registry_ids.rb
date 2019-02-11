5ca7e4aacebd46216e076cb468ed27d78b266e64
Susanna Kiwala <ssiebert@genome.wustl.edu>
2019-02-11 09:06:10 -0600

    Upload linkage to Allele Registry for CIViC variants that have an allele registry ID

--- app/jobs/update_allele_registry_ids.rb
+++ app/jobs/update_allele_registry_ids.rb
@@ -2,12 +2,25 @@ class UpdateAlleleRegistryIds < ActiveJob::Base
   def perform(recurring = true)
     begin
       for v in Variant.where(allele_registry_id: nil) do
-        v.allele_registry_id = get_allele_registry_id(v)
+        allele_registry_id = get_allele_registry_id(v)
+        v.allele_registry_id = allele_registry_id
         v.save
+      end
+      for v in Variant.where.not(allele_registry_id: nil) do
+        allele_registry_id = get_allele_registry_id(v)
+        add_allele_registry_link(allele_registry_id)
       end
     ensure
       reschedule if recurring
     end
+  end
+
+  def add_allele_registry_link(allele_registry_id)
+    system("ruby #{script_path} put \"reg.test.genome.network/allele/#{allele_registry_id}/externalSource/civic\" \"p1=#{allele_registry_id}\" #{allele_registry_username} #{allele_registry_password}")
+  end
+
+  def script_path
+    File.join(Rails.root, 'misc_scripts', 'add_allele_registry_link.rb')
   end
 
   def reschedule
@@ -41,5 +54,13 @@ class UpdateAlleleRegistryIds < ActiveJob::Base
 
   def allele_registry_url(coordinate_string)
     URI.encode("http://reg.genome.network/allele?hgvs=#{coordinate_string}")
+  end
+
+  def allele_registry_username
+    ENV['ALLELE_REGISTRY_USERNAME'] || Rails.application.secrets.allele_registry_username
+  end
+
+  def allele_registry_password
+    ENV['ALLELE_REGISTRY_PASSWORD'] || Rails.application.secrets.allele_registry_password
   end
 end
