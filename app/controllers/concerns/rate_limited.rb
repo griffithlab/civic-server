96adf5ee9b5ddf198fefa4365838a97a95c3e4b1
Adam Coffman <acoffman@wustl.edu>
2019-04-26 12:42:18 -0500

    dont attach headers in testing or dev

--- app/controllers/concerns/rate_limited.rb
+++ app/controllers/concerns/rate_limited.rb
@@ -6,10 +6,12 @@ module RateLimited
   end
 
   def add_rate_limit_headers
-    throttle_data = request.env['rack.attack.throttle_data']['req/ip']
-    now = throttle_data[:epoch_time]
-    headers['RateLimit-Limit'] = throttle_data[:limit].to_s
-    headers['RateLimit-Remaining'] = (throttle_data[:limit] - throttle_data[:count]).to_s
-    headers['RateLimit-Reset'] = (now + (throttle_data[:period] - now % throttle_data[:period])).to_s
+    if Rails.env.production?
+      throttle_data = request.env['rack.attack.throttle_data']['req/ip']
+      now = throttle_data[:epoch_time]
+      headers['RateLimit-Limit'] = throttle_data[:limit].to_s
+      headers['RateLimit-Remaining'] = (throttle_data[:limit] - throttle_data[:count]).to_s
+      headers['RateLimit-Reset'] = (now + (throttle_data[:period] - now % throttle_data[:period])).to_s
+    end
   end
 end
