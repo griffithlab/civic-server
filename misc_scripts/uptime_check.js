0e29d81a971f03ececc75081acec43092eb344d4
Adam Coffman <acoffman@wustl.edu>
2019-04-26 17:22:12 -0500

    Initial alert backoff, using s3 for state

--- misc_scripts/uptime_check.js
+++ misc_scripts/uptime_check.js
@@ -1,5 +1,5 @@
 let https = require('https');
-
+let aws = require('aws-sdk');
 
 let sendSlackNotification = (message) => {
   let timestamp = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
@@ -32,16 +32,53 @@ let isSiteDown = (url, callback) => {
   req.setTimeout(2500, () => { callback(true) });
 };
 
+let fileContents = (timestamp, retries) => { `${timestamp}\t${retries}` };
+
+let retryCount = (fileContents) => { parseInt(fileContents.split("\t")[1], 10) };
+let lastAlert = (fileContents) => { new Date(parseInt(fileContents.split("\t")[0], 10) * 1000) };
+
+let secondsSinceEpoch = () => { Math.floor(new Date().getTime() / 1000) };
+let backOffSeconds = 15 * 60;
 
 exports.handler = (event) => {
+  let s3 = new aws.S3();
+
+  let s3Params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: process.env.SERVICE_NAME
+  };
+
   let testUrl = "https://civicdb.org/api/server";
-  isSiteDown(testUrl, (isDown) => {
+
+  isSiteDown(testUrl, async function(isDown) {
     if (isDown) {
-      sendSlackNotification('<!channel> CIViC Appears to be down');
+      s3.getObject(s3Params, (err, data) => {
+        if (err) {
+          console.log(err);
+          sendSlackNotification('<!channel> CIViC Appears to be down');
+        } else {
+          let contents = data.Body.toString('ascii');
+          if (contents == "OK") {
+            s3Params.Body = fileContents(secondsSinceEpoch(),'1');
+            sendSlackNotification('<!channel> CIViC Appears to be down');
+          } else {
+            let lastAlertTime = lastAlert(contents);
+            let retryNumber = retryCount(contents);
+            if (secondsSinceEpoch > (lastAlertTime + (retryNumber * backOffSeconds))) {
+              s3Params.Body = fileContents(secondsSinceEpoch(), retryNumber + 1);
+              sendSlackNotification('<!channel> CIViC Still appears to be down');
+            } else {
+              s3Params.Body = fileContents(lastAlertTime, retryNumber + 1);
+            }
+          }
+        }
+      });
     } else {
-      console.log('all good');
+      s3Params.Body = 'OK';
     }
+    await s3.putObject(s3Params).promise();
   });
+
 
   const response = {
     statusCode: 200,
