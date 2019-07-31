1bf06c3910e84316b96277209e4c47faf6122150
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-07-31 11:11:17 -0500

    Fix typos

--- app/jobs/update_allele_registry_ids.rb
+++ app/jobs/update_allele_registry_ids.rb
@@ -8,7 +8,7 @@ class UpdateAlleleRegistryIds < AlleleRegistryIds
         allele_registry_id = get_allele_registry_id(v)
         if allele_registry_id != old_allele_registry_id
           if allele_registry_id == '_:CA'
-            v.allele_registry_is = 'unregistered'
+            v.allele_registry_id = 'unregistered'
             v.save
           else
             v.allele_registry_id = allele_registry_id
@@ -17,7 +17,7 @@ class UpdateAlleleRegistryIds < AlleleRegistryIds
           end
           #delete the linkout if no other variant has this allele registry ID
           unless Variant.where(allele_registry_id: old_allele_registry_id).exists?
-            if old_allele_registry != '_:CA' and old_allele_registry != 'undefined'
+            if old_allele_registry_id != '_:CA' and old_allele_registry_id != 'undefined'
               delete_allele_registry_link(old_allele_registry_id)
             end
           end
