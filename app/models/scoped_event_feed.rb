26a4544aa79f07f1275a905811690a5349754c60
Adam Coffman <acoffman@wustl.edu>
2019-08-28 15:00:34 -0500

    Stop manually generating SQL

--- app/models/scoped_event_feed.rb
+++ app/models/scoped_event_feed.rb
@@ -11,16 +11,8 @@ class ScopedEventFeed
     if @events
       @events
     else
-      event_ids = event_subjects.group_by { |x| x.class.to_s }
-      sql_segments = []
-      sql_params = []
-      event_ids.each do|(klass, members)|
-        sql_segments << "(events.subject_id IN (?) AND events.subject_type = ?)"
-        sql_params << members.map(&:id)
-        sql_params << klass
-      end
       @events = Event.includes(:subject, originating_user: [:organization])
-        .where(sql_segments.join(" OR "), *sql_params)
+        .where(subject: event_subjects)
         .order('events.created_at desc')
         .page(page)
         .per(per)
@@ -38,6 +30,5 @@ class ScopedEventFeed
         h[comment.id] = comment
       end
     end
-
   end
 end
