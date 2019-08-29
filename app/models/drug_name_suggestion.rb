a1a8a03e4fbff14b26dea478292dd1d749f56c8b
Adam Coffman <acoffman@wustl.edu>
2019-08-29 16:09:52 -0500

    Limit drug name suggestions
    Only return local name suggestions in the typeahead for drugs already
    associated with an accepted evidence item.

--- app/models/drug_name_suggestion.rb
+++ app/models/drug_name_suggestion.rb
@@ -17,8 +17,11 @@ class DrugNameSuggestion
   end
 
   def self.get_local_suggestions(name)
-    Drug.where('name ILIKE :name', name: "%#{name}%")
+
+    Drug.where('name ILIKE :name', name: "%#{name}%", evidence_items: {status: 'accepted'})
+      .joins(:evidence_items)
       .limit(12)
+      .distinct
       .pluck(:name)
   end
 
