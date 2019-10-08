8f8c17805370e66349c2e6dfbcf180b93f0e5794
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-10-08 10:41:39 -0500

    Update create_civic_vcf job to use new civicpy cli commands

--- app/jobs/create_civic_vcf.rb
+++ app/jobs/create_civic_vcf.rb
@@ -1,15 +1,11 @@
 class CreateCivicVcf < ActiveJob::Base
   def perform
-    system("python3 #{script_path} #{vcf_path}")
+    system("civicpy create-vcf --vcf-file-path #{vcf_path} --include-status accepted --include-status submitted")
     self.class.set(wait_until: Date.tomorrow.midnight).perform_later
   end
 
-  def script_path
-    File.join(Rails.root, 'misc_scripts', 'create_civic_vcf.py')
-  end
-
   private
-  def civicpy_cache_file_location
+  def vcf_path
     File.join(Rails.root, 'public', 'downloads', 'civic.vcf')
   end
 end
