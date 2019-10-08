b9260646c50a8bd72cd30ae77235218a9c70ac68
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-10-08 13:54:06 -0500

    Amend the VCF creation job to create VCFs for multiple statuses
    We want one VCF with only accepted entities, and one with only accepted
    and submitted entities

--- app/jobs/create_civic_vcf.rb
+++ app/jobs/create_civic_vcf.rb
@@ -1,11 +1,25 @@
 class CreateCivicVcf < ActiveJob::Base
   def perform
-    system("civicpy create-vcf --vcf-file-path #{vcf_path} --include-status accepted --include-status submitted")
-    self.class.set(wait_until: Date.tomorrow.midnight).perform_later
+    statuses.each do |description, status_list|
+      binding.pry
+      cmd = "civicpy create-vcf --vcf-file-path #{vcf_path(description)}"
+      status_list.each do |status|
+        cmd += " --include-status #{status}"
+      end
+      system(cmd)
+      self.class.set(wait_until: Date.tomorrow.midnight).perform_later
+    end
   end
 
   private
-  def vcf_path
-    File.join(Rails.root, 'public', 'downloads', 'civic.vcf')
+  def vcf_path(description)
+    File.join(Rails.root, 'public', 'downloads', "civic_#{description}.vcf")
+  end
+
+  def statuses
+    {
+      'accepted' => ['accepted'],
+      'accepted_and_submitted' => ['accepted', 'submitted'],
+    }
   end
 end
