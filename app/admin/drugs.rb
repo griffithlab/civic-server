fff4a2cf900486e372a2663dd098e493a8c1ef97
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-01-06 08:34:14 -0600

    Remove pubchem_id from drugs table

--- app/admin/drugs.rb
+++ app/admin/drugs.rb
@@ -1,16 +1,14 @@
 ActiveAdmin.register Drug do
   menu priority: 7
-  permit_params :pubchem_id, :name, :ncit_id
+  permit_params :name, :ncit_id
 
   filter :name
-  filter :pubchem_id
   filter :ncit_id
 
   form do |f|
     f.semantic_errors(*f.object.errors.keys)
     f.inputs do
       f.input :name
-      f.input :pubchem_id
       f.input :ncit_id, input_html: { rows: 1 }
     end
     f.actions
@@ -19,7 +17,6 @@ ActiveAdmin.register Drug do
   index do
     selectable_column
     column :name
-    column :pubchem_id
     column :ncit_id
     column :created_at
     column :updated_at
@@ -29,7 +26,6 @@ ActiveAdmin.register Drug do
   show do |f|
     attributes_table do
       row :name
-      row :pubchem_id
       row :ncit_id
       row :evidence_item_count do |d|
         d.evidence_items.count
--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -1,12 +1,12 @@
 class DrugsController < ApplicationController
 
-  actions_without_auth :index, :existence, :name_suggestion, :local_name_suggestion
+  actions_without_auth :index, :name_suggestion, :local_name_suggestion
 
   def index
     drugs = Drug.page(params[:page])
       .per(params[:count])
 
-    drugs = name_search(pubchem_id_search(drugs))
+    drugs = name_search(drugs)
 
     render json: drugs.map { |d| DrugPresenter.new(d) }
   end
@@ -21,20 +21,6 @@ class DrugsController < ApplicationController
       [d, :created]
     end
     render json: DrugPresenter.new(drug), status: status
-  end
-
-  def existence
-    proposed_pubchem_id = params[:pubchem_id]
-    (to_render, status) = if drug = Drug.find_by(pubchem_id: proposed_pubchem_id)
-      [{ name: drug.name, pubchem_id: drug.pubchem_id }, :ok]
-    else drug_name = Scrapers::PubChem.get_name_from_pubchem_id(proposed_pubchem_id)
-      if drug_name.present?
-        [{ name: drug_name, pubchem_id: proposed_pubchem_id }, :ok]
-      else
-        [{}, :not_found]
-      end
-    end
-    render json: to_render, status: status
   end
 
   def name_suggestion
@@ -62,14 +48,6 @@ class DrugsController < ApplicationController
   def name_search(query)
     if params[:name].present?
       query.where('drugs.name ILIKE :name', name: "#{params[:name]}%")
-    else
-      query
-    end
-  end
-
-  def pubchem_id_search(query)
-    if params[:pubchem_id].present?
-      query.where('drugs.pubchem_id ILIKE :pubchem_id', pubchem_id: "#{params[:pubchem_id]}%")
     else
       query
     end
