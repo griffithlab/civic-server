7d788f54b5077d3486322d0b4c0afe2c06181dab
Adam Coffman <acoffman@wustl.edu>
2020-01-31 11:10:35 -0600

    Handle coordinates passed in with commas
    This adds a new mixin that can be generically used anywhere we're
    getting a string from the frontend that we eventually cast to a number.
    
    When mixed in, it defines a class method that allows you to specify
    which columns require this treatment. The specified columns will have
    their default setter method overidden to remove commas before the
    ActiveRecord column casting logic.
    
    closes griffithlab/civic-client#1315

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -7,6 +7,7 @@ class Variant < ActiveRecord::Base
   include WithDomainExpertTags
   include Flaggable
   include Commentable
+  include WithStringToIntColumns
 
   belongs_to :gene
   belongs_to :secondary_gene, class_name: 'Gene'
@@ -24,6 +25,8 @@ class Variant < ActiveRecord::Base
   enum reference_build: [:GRCh38, :GRCh37, :NCBI36]
 
   after_initialize :init
+
+  string_to_int_columns :start, :start2, :stop, :stop2
 
   def init
     self.civic_actionability_score ||= 0 if self.has_attribute? :civic_actionability_score
