ea31b9c3d0c53b7a193e728be173561437ba8e5d
Adam Coffman <acoffman@wustl.edu>
2020-04-15 12:11:41 -0500

    update tests to make the user a member of the org

--- app/models/user_context.rb
+++ app/models/user_context.rb
@@ -2,11 +2,8 @@ class UserContext < DelegateClass(User)
   attr_reader :acting_as_organization_id
 
   def initialize(user, organization_id)
-    @acting_as_organization_id = organization_id
+    numeric_id = Integer(organization_id) rescue nil
+    @acting_as_organization_id = numeric_id
     super(user)
-  end
-
-  def acting_as_organization
-    Organization.find(acting_as_organization_id)
   end
 end
