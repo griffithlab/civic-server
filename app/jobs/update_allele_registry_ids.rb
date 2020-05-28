fabb19d7451f88de4c6a2dbcd4c8b49d67aa18db
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-05-28 12:59:22 -0500

    Better conditional

--- app/jobs/update_allele_registry_ids.rb
+++ app/jobs/update_allele_registry_ids.rb
@@ -10,7 +10,7 @@ class UpdateAlleleRegistryIds < AlleleRegistryIds
         if allele_registry_id == '_:CA'
           v.allele_registry_id = 'unregistered'
           v.save
-        elsif not allele_registry_id.nil?
+        elsif allele_registry_id.present?
           v.allele_registry_id = allele_registry_id
           v.save
           add_allele_registry_link(allele_registry_id)
