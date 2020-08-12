bf23c08c0e8eada61c1ac9feaf4d8204860a8c46
Adam Coffman <acoffman@wustl.edu>
2020-08-12 18:26:02 -0500

    fix failing test, include into correct namespace

--- app/models/concerns/with_stripped_whitespace.rb
+++ app/models/concerns/with_stripped_whitespace.rb
@@ -1,7 +1,7 @@
 module WithStrippedWhitespace
   extend ActiveSupport::Concern
-  include SetterOverrideMixin
   class_methods do
+    include SetterOverrideMixin
     def columns_with_stripped_whitespace(*cols)
       cols.each do |col|
         safe_override_setter(col, 'squished') do |val|
