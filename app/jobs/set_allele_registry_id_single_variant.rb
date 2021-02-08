494ef45539ffdc8db27742421681d384ffb7f23f
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-02-08 10:26:35 -0600

    Fix logic of when to set the allele registry id

--- app/jobs/set_allele_registry_id_single_variant.rb
+++ app/jobs/set_allele_registry_id_single_variant.rb
@@ -4,15 +4,13 @@ class SetAlleleRegistryIdSingleVariant < AlleleRegistryIds
 
   def perform(variant)
     allele_registry_id = get_allele_registry_id(variant)
-    if allele_registry_id != variant.allele_registry_id
-      if allele_registry_id == '_:CA'
-        variant.allele_registry_id = 'unregistered'
-        variant.save
-      elsif allele_registry_id.present?
-        variant.allele_registry_id = allele_registry_id
-        variant.save
-        add_allele_registry_link(allele_registry_id)
-      end
+    if allele_registry_id == '_:CA' && variant.allele_registry_id != 'unregistered'
+      variant.allele_registry_id = 'unregistered'
+      variant.save
+    elsif allele_registry_id.present? && allele_registry_id != variant.allele_registry_id
+      variant.allele_registry_id = allele_registry_id
+      variant.save
+      add_allele_registry_link(allele_registry_id)
     end
   end
 end
