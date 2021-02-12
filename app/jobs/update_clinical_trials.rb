9d1cff70e0fe6087ab5da9444f2650ce2c134335
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-02-12 10:39:23 -0600

    Speed up clinical trial processing

--- app/jobs/update_clinical_trials.rb
+++ app/jobs/update_clinical_trials.rb
@@ -1,13 +1,15 @@
 class UpdateClinicalTrials < ApplicationJob
 
   def perform
-    Source.where(source_type: 'PubMed').each do |source|
+    Source.eager_load(:clinical_trials).where(source_type: 'PubMed').each do |source|
       resp = Scrapers::PubMed.call_pubmed_api(source.citation_id)
       clinical_trials = resp.clinical_trial_ids.uniq.map do |nct_id|
         ClinicalTrial.where(nct_id: nct_id).first_or_create
       end
-      source.clinical_trials = clinical_trials
-      source.save
+      if clinical_trials.sort != source.clinical_trials.sort
+        source.clinical_trials = clinical_trials
+        source.save
+      end
       sleep 1
     end
   end
