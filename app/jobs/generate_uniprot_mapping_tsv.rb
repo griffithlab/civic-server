3cb339316f2c894de8855e9d02028aa8d2768035
dorian <dojmorrissey@gmail.com>
2021-09-10 10:44:35 -0500

    Implemented sugestions in pull request

--- app/jobs/generate_uniprot_mapping_tsv.rb
+++ app/jobs/generate_uniprot_mapping_tsv.rb
@@ -1,54 +1,45 @@
-class GenerateUniprotMappingTsv < GenerateTsvs
-    def perform
-      ensure_downloads_directory_exists
-      tsvs_to_generate.each do |e|
-        begin
-          tmp_file = tmp_file(e.file_name)
-          tmp_file.puts(e.headers.join("\t"))
-  
-          e.objects.find_each do |object|
-            
-            row = e.row_from_object(object)
-            if row[1].is_a?(Array)
-              overview_col = e.formatted_overview_col(object)
-              row[1].each do |r|
-                new_row = [row[0], r, overview_col].join("\t")
-                tmp_file.puts(new_row)
-              end
-            elsif row[1] == "N/A"
-              next
-            else
-              tmp_file.puts(row.join("\t"))
-            end
-          end
-  
-          tmp_file.close
-          public_path = public_file_path(e.file_name)
-          FileUtils.cp(tmp_file.path, public_path)
-          File.chmod(0644, public_path)
-        ensure
-          tmp_file.unlink
+class GenerateUniprotMappingTsv < ApplicationJob
+  def perform
+    ensure_downloads_directory_exists
+    p = UniprotMappingTsvPresenter
+    begin
+      tmp_file = tmp_file(p.file_name)
+      tmp_file.puts(p.headers.join("\t"))
+
+      Gene.joins(variants: :evidence_items).distinct.find_each do |gene|  
+        rows = p.rows_from_object(gene)
+
+        rows.each do |r|
+          tmp_file.puts(r.join("\t"))
         end
       end
-    end
-  
-    def tsvs_to_generate
-      [UniprotMappingTsvPresenter]
-    end
-  
-    def public_file_path(filename)
-      File.join(downloads_dir_path, filename)
-    end
-  
-    def downloads_dir_path
-      TsvRelease.downloads_path
-    end
-  
-    def release_path
-      ''
-    end
-  
-    def filename_prefix
-      ''
+      tmp_file.close
+      public_path = public_file_path(p.file_name)
+      FileUtils.cp(tmp_file.path, public_path)
+      File.chmod(0644, public_path)
+    ensure
+      tmp_file.unlink
     end
   end
+
+  private
+  def tmp_file(filename)
+    Tempfile.new(filename, File.join(Rails.root, 'tmp'))
+  end
+
+  def ensure_downloads_directory_exists
+    FileUtils.mkdir_p(downloads_dir_path)
+  end
+  
+  def tsvs_to_generate
+    [UniprotMappingTsvPresenter]
+  end
+
+  def public_file_path(filename)
+    File.join(downloads_dir_path, filename)
+  end
+
+  def downloads_dir_path
+    TsvRelease.downloads_path
+  end
+end
