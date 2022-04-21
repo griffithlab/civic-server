6dea64252797453adee7cefa8c44c23f186034ca
Ruslan Forostianov <Ruslan.Forostyanov@m2gen.com>
2022-04-21 08:08:29 +0200

    Use the docker entry point

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
 FROM ruby:2
 
+ENV RAILS_ENV=production
 RUN apt-get update
 RUN apt-get install -y rbenv libxml2 libxslt-dev openssl nodejs postgresql-client
+RUN gem install bundler
+
+#RUN mkdir /civic-server
+#RUN cd /civic-server && bundle install
+
+COPY Gemfile /civic-server/Gemfile
+COPY Gemfile.lock /civic-server/Gemfile.lock
+
+RUN cd /civic-server && bundle update --bundler && rbenv rehash && bundle install
+
 COPY $PWD /civic-server
 WORKDIR /civic-server
-RUN gem install bundler
-RUN bundle update --bundler
-RUN rbenv rehash
-RUN bundle install
-RUN rbenv rehash
+
+ENTRYPOINT ["/civic-server/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,21 +4,28 @@ set -e
 set -u # unset variables throw error
 set -o pipefail # pipes fail when partial command fails
 
+bundle update --bundler 
 # make sure env $DATABASE_* items are set!
 # Attempt connect to db:
-psql postgresql://[$DATABASE_USER[:$DATABASE_PASSWORD]@][$DATABASE_HOST][:5432][/civic]
+# psql postgresql://[$DATABASE_USER[:$DATABASE_PASSWORD]@][$DATABASE_HOST][:5432][/civic]
 
 # If it fails, then assume db does not exist and
 # db creation should be executed:
 # TODO - add IF
-if ....
+if PGPASSWORD=$DATABASE_PASSWORD psql -lqt -U$DATABASE_USER -h$DATABASE_HOST | cut -d \| -f 1 | grep -qw civic; then
+    echo civic database already exists. Skipping database initialisation.
+else
+    echo Creating civic database
     rake db:create
 
+    echo Migrating civic database
     # migrate:
     rake db:migrate
 
+    echo Loading a recent database backup
     # load the sanitized version of a recent database backup found in ./db:
     rake civic:load[force]
+    echo Done
 fi
 
 rails s
